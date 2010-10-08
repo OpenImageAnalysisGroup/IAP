2c13198fc90dbc6c8271767a893e5fd08aa303b2
Dr. Christian Klukas <cklukas@users.noreply.github.com>
2010-10-08 07:35:44 +0000

    *** empty log message ***

--- IAP/src/de/ipk_gatersleben/ag_ba/postgresql/DataExchange.java
+++ IAP/src/de/ipk_gatersleben/ag_ba/postgresql/DataExchange.java
@@ -11,213 +11,133 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-
 
 /**
- * @author entzian
- *
+ * @author entzian, klukas
+ * 
  */
 public class DataExchange {
 
 	/**
 	 * @param args
 	 */
-	
-	
-	
-	
+
 	// Zum testen werden hier die Zugangsdaten eingetragen
-	// sollte spŠter anders abgelegt werden
-	
-	private String password = "LemnaTec";
-	private String user = "postgres";
-	private String port = "5432";
-	private String host = "192.168.32.203";
-	private String driver = "org.postgresql.Driver";
-	private String database;
-	private Connection connection = null;
-	
-	private ArrayList<String> DB_Exp_Data;
-	
-	private String experiment[];	//das kann dann weg
-	
-	
-	DataExchange()
-	{
+	// sollte spï¿½ter anders abgelegt werden
+
+	private final String password = "LemnaTec";
+	private final String user = "postgres";
+	private final String port = "5432";
+	private final String host = "192.168.32.203";
+	private final String driver = "org.postgresql.Driver";
+
+	// private ArrayList<String> DB_Exp_Data;
+
+	private String experiment[]; // das kann dann weg
+
+	DataExchange() {
 		this("DH-MB1");
 	}
-	
-	DataExchange(String database)
-	{
-		this.database = database;	
-		loadJdbcDriver();
-		openConnection();
-		ExperimentDerDatenbankAuslesen();
-		closeConnection();
-	}
-	
-	
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		
-		//ExperimentHeader H1 = new ExperimentHeader();
-		//H1.setExperimentLable("hallo");
-		//H1.setExperimentLable(this.getString(mesuremtn_lable));
-		
+	DataExchange(String database) {
+		try {
+			loadJdbcDriver();
+			Connection connection = openConnection(database);
+			ExperimentDerDatenbankAuslesen(connection);
+			closeConnection(connection);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		// ExperimentHeader H1 = new ExperimentHeader();
+		// H1.setExperimentLable("hallo");
+		// H1.setExperimentLable(this.getString(mesuremtn_lable));
+
 		DataExchange Test = new DataExchange("DH-MB1");
 		String testArray[] = Test.getExperimentLabels();
-		
-		if(DF.TEST)
-			DF.print("Grš§e des TestArrays: ", testArray.length);
-		
-		
-		for (int za = testArray.length-1; za >=0; za--)
+
+		if (Debug.TEST)
+			Debug.print("GrÃ¶ÃŸe des TestArrays: ", testArray.length);
+
+		for (int za = testArray.length - 1; za >= 0; za--)
 			System.out.println(testArray[za]);
-		
+
 	}
-	
+
 	/*
-	private ExperimentHeader H1[];
-	
-	public String test()
-	{
-	
-	H1[ZeilenAnzahl] = new ExperimentHeader();
-	for(int anzahl = ZeilenAnzahl; anzahl >= 0; anzahl--)
-	ExperimentHeader H1[anzahl]
-	
-	
-		return result;
-	}
-	
-	*/
-	
-	public String[] getExperimentLabels ()
-	{
+	 * private ExperimentHeader H1[];
+	 * 
+	 * public String test() {
+	 * 
+	 * H1[ZeilenAnzahl] = new ExperimentHeader(); for(int anzahl = ZeilenAnzahl;
+	 * anzahl >= 0; anzahl--) ExperimentHeader H1[anzahl]
+	 * 
+	 * 
+	 * return result; }
+	 */
+
+	public String[] getExperimentLabels() {
 		return experiment;
 	}
-	
-	public String getSingelExperimentLabel (int position)
-	{
-		if(position < 0 || position > experiment.length)
+
+	public String getSingelExperimentLabel(int position) {
+		if (position < 0 || position > experiment.length)
 			return "";
 		else
 			return experiment[position];
 	}
-	
-	
-	
-	
-	
-	private void ExperimentDerDatenbankAuslesen()
-	{
-				
-		try {
-			
-			
-			if(DF.TEST)
-			{
-				DatabaseMetaData meta = connection.getMetaData(); //Metadata abfragen
-				DF.print("Connection successful:", meta.getDatabaseProductName()+" "+meta.getDatabaseProductVersion());
-			}
-			
-			
-			Statement statm = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, 0);	
 
-			String sqlText = "Select distinct measurement_label from import_data;";
-			
-			if(DF.TEST)
-				DF.print(sqlText);
-			
-			ResultSet result = statm.executeQuery(sqlText);
-		
-			
-			result.last();
-			int rows = result.getRow();
-			result.beforeFirst();
-			
-			if(DF.TEST)
-				DF.print("LŠnge result:", rows);
-			
-			
-			experiment = new String[rows];
-			
-			while(result.next())
-			{
-				if(DF.TEST)
-				{
-					//print("aktuelle Zeile: ", String.valueOf(result.getRow()));
-					DF.print("aktuelle Zeile: ", result.getString(1));
-				}
-				
-				experiment[result.getRow()-1] = result.getString(1);
-			
+	private void ExperimentDerDatenbankAuslesen(Connection connection) throws SQLException {
+		if (Debug.TEST) {
+			DatabaseMetaData meta = connection.getMetaData(); // Metadata
+			// abfragen
+			Debug.print("Connection successful:", meta.getDatabaseProductName() + " " + meta.getDatabaseProductVersion());
+		}
+
+		Statement statm = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, 0);
+
+		String sqlText = "Select distinct measurement_label from import_data";
+
+		if (Debug.TEST)
+			Debug.print(sqlText);
+
+		ResultSet result = statm.executeQuery(sqlText);
+
+		result.last();
+		int rows = result.getRow();
+		result.beforeFirst();
+
+		if (Debug.TEST)
+			Debug.print("LÃ¤nge result:", rows);
+
+		experiment = new String[rows];
+
+		while (result.next()) {
+			if (Debug.TEST) {
+				Debug.print("aktuelle Zeile: ", result.getString(1));
 			}
-			result.close();
-			statm.close();			
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+			experiment[result.getRow() - 1] = result.getString(1);
+
 		}
-		
+		result.close();
+		statm.close();
 	}
-	
-	
-	
-	
-	/**
-	 * load Driver
-	 */
-	
-	private void loadJdbcDriver()
-	{
-		try
-		{
-			Class.forName(driver);
-		}
-		catch(ClassNotFoundException e)
-		{
-			e.printStackTrace();
-		}
+
+	private void loadJdbcDriver() throws ClassNotFoundException {
+		Class.forName(driver);
 	}
-	
-	/**
-	 * open connection to DB
-	 */
-	
-	private void openConnection()
-	{
-		try
-		{
-			String path = "jdbc:postgresql:" + (host != null ? ("//" + host ) + (port != null ? ":" + port : "") + "/" : "") + database;
-			connection = DriverManager.getConnection(path, user, password);
-		}
-		catch(SQLException e)
-		{
-			e.printStackTrace();
-		}
+
+	private Connection openConnection(String database) throws SQLException {
+		String path = "jdbc:postgresql:" + (host != null ? ("//" + host) + (port != null ? ":" + port : "") + "/" : "")
+				+ database;
+		return DriverManager.getConnection(path, user, password);
 	}
-	
-	
-	/**
-	 * close connection
-	 */
-	
-	private void closeConnection()
-	{
-		try
-		{
-			connection.close();
-		}
-		catch(SQLException e)
-		{
-			e.printStackTrace();
-		}
+
+	private void closeConnection(Connection connection) throws SQLException {
+		connection.close();
 	}
-	
+
 }
--- IAP/src/de/ipk_gatersleben/ag_ba/postgresql/Debug.java
+++ IAP/src/de/ipk_gatersleben/ag_ba/postgresql/Debug.java
@@ -7,37 +7,25 @@ package de.ipk_gatersleben.ag_ba.postgresql;
 
 /**
  * @author entzian
- *
  */
-public class DF {
-	
+public class Debug {
+
 	public static final boolean TEST = true;
-	
-	public DF()
-	{
-		
+
+	public static final void print(String text) {
+		print("Executing this command: ", text);
 	}
 
-	public static final void print(String text)
-	{
-		print("Executing this command: ", text);
-		
-	}
-	
-	public static final void print(String text1, String text2)
-	{
+	public static final void print(String text1, String text2) {
 		System.out.println(text1 + text2 + "\n");
 	}
-	
-	public static final void print(String text1, int zahl1)
-	{
+
+	public static final void print(String text1, int zahl1) {
 		print(text1, String.valueOf(zahl1));
 	}
 
-	public static final void print(String text1, boolean b1)
-	{
+	public static final void print(String text1, boolean b1) {
 		print(text1, String.valueOf(b1));
 	}
-
 
 }
