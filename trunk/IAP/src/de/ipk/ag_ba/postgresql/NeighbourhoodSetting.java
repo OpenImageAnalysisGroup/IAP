ab57d02fad5ebca835ae0414b36d39bf509e15ce
klukas <klukas@90d7db82-dce4-4103-b106-19f43c5924aa>
2010-10-29 12:48:00 +0000

    *** empty log message ***

---
+++ trunk/IAP/src/de/ipk/ag_ba/postgresql/NeighbourhoodSetting.java
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * 
+ *    Copyright (c) 2010 Image Analysis Group, IPK Gatersleben
+ * 
+ *******************************************************************************/
+/*
+ * Created on Oct 29, 2010 by Christian Klukas
+ */
+package de.ipk.ag_ba.postgresql;
+
+/**
+ * @author klukas
+ * 
+ */
+public enum NeighbourhoodSetting {
+	NB4, NB8
+}
--- trunk/IAP/src/de/ipk/ag_ba/postgresql/TestSegmentation.java
+++ trunk/IAP/src/de/ipk/ag_ba/postgresql/TestSegmentation.java
@@ -9,73 +9,74 @@ public class TestSegmentation {
 
 	@Test
 	public void test() {
-	
-		int [][] eingabe_image1 = { { 0, 1 },
-								   { 0, 0 } }	;
-		
-		int [][] eingabe_image2 = { { 0, 1, 1 },
-								   { 1, 0, 0 } };
-		
-		int [][] eingabe_image3 = { { 0, 0, 0 },
-								   { 0, 0, 0 } };
-		
-		int [][] eingabe_image4 = { { 1, 1, 1 },
-								   { 1, 1, 1 } };
-		
-		int [][] eingabe_image5 = { { 0, 1, 1, 0, 1, 0, 1, 0 },
-								   { 1, 1, 0, 0, 1, 1, 1, 0 },
-								   { 0, 1, 1, 1, 1, 0, 1, 0 },
-								   { 0, 0, 0, 0, 0, 1, 1, 0 },
-								   { 0, 1, 1, 1, 0, 0, 0, 1 },
-								   { 1, 1, 1, 1, 1, 0, 0, 0 } };
-		
-		ArrayList<int [][]> testImage = new ArrayList<int [][]>();
+
+		int[][] eingabe_image1 = { { 0, 1 }, { 0, 0 } };
+
+		int[][] eingabe_image2 = { { 0, 1, 1 }, { 1, 0, 0 } };
+
+		int[][] eingabe_image3 = { { 0, 0, 0 }, { 0, 0, 0 } };
+
+		int[][] eingabe_image4 = { { 1, 1, 1 }, { 1, 1, 1 } };
+
+		int[][] eingabe_image5 = { { 0, 1, 1, 0, 1, 0, 1, 0 }, { 1, 1, 0, 0, 1, 1, 1, 0 }, { 0, 1, 1, 1, 1, 0, 1, 0 },
+				{ 0, 0, 0, 0, 0, 1, 1, 0 }, { 0, 1, 1, 1, 0, 0, 0, 1 }, { 1, 1, 1, 1, 1, 0, 0, 0 } };
+
+		ArrayList<int[][]> testImage = new ArrayList<int[][]>();
 		testImage.add(eingabe_image1);
 		testImage.add(eingabe_image2);
 		testImage.add(eingabe_image3);
 		testImage.add(eingabe_image4);
 		testImage.add(eingabe_image5);
-		
-		//4er ClusterCount
-		int [] testCluster4er = new int [5];
+
+		// 4er ClusterCount
+		int[] testCluster4er = new int[5];
 		testCluster4er[0] = 1;
 		testCluster4er[1] = 2;
 		testCluster4er[2] = 0;
 		testCluster4er[3] = 1;
 		testCluster4er[4] = 3;
-		
-		//8er ClusterCount
-		int [] testCluster8er = new int [5];
+
+		// 8er ClusterCount
+		int[] testCluster8er = new int[5];
 		testCluster8er[0] = 1;
 		testCluster8er[1] = 1;
 		testCluster8er[2] = 0;
 		testCluster8er[3] = 1;
 		testCluster8er[4] = 2;
-		
-		//4/8er PixelCount
-		int [] testPixel = new int [5];
+
+		// 4/8er PixelCount
+		int[] testPixel = new int[5];
 		testPixel[0] = 1;
 		testPixel[1] = 3;
 		testPixel[2] = 0;
 		testPixel[3] = 6;
 		testPixel[4] = 25;
-			
-		
-		pixel_segmentierung test;
-		int [][] image;
-		
-		for (int i = 0; i < testImage.size(); i++){
+
+		PixelSegmentation test;
+		int[][] image;
+
+		for (int i = 0; i < testImage.size(); i++) {
 			image = testImage.get(i);
-			
-			test = new pixel_segmentierung(image, false);
+
+			test = new PixelSegmentation(image, NeighbourhoodSetting.NB4);
 			test.doPixelSegmentation();
 			Assert.assertEquals(test.getNumberOfCluster(), testCluster4er[i]);
 			Assert.assertEquals(test.getNumberOfPixel(), testPixel[i]);
-			
-			test = new pixel_segmentierung(image, true);
+
+			test = new PixelSegmentation(image, NeighbourhoodSetting.NB4);
 			test.doPixelSegmentation();
 			Assert.assertEquals(test.getNumberOfCluster(), testCluster8er[i]);
 			Assert.assertEquals(test.getNumberOfPixel(), testPixel[i]);
+
+			// PixelSegmentation test = new PixelSegmentation(eingabe_image,
+			// NeighbourhoodSetting.NB4);
+			// test.doPixelSegmentation();
+			// test.printOriginalImage();
+			// test.printImage();
+			// test.printClusterArray();
+			// System.out.println("Number of Clusters: " +
+			// test.getNumberOfCluster());
+			// System.out.println("Number of Pixel: " + test.getNumberOfPixel());
 		}
 	}
 }
