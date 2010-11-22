7478588b8cbfb4e2058cf3353246c576cd78cd5a
klukas <klukas@90d7db82-dce4-4103-b106-19f43c5924aa>
2010-11-22 15:43:49 +0000

    *** empty log message ***

--- trunk/IAP/src/de/ipk/ag_ba/rmi_server/analysis/image_analysis_tasks/reconstruction3d/MyPicture.java
+++ trunk/IAP/src/de/ipk/ag_ba/rmi_server/analysis/image_analysis_tasks/reconstruction3d/MyPicture.java
@@ -1,12 +1,16 @@
 /*************************************************************************************
  * The HIVE Add-on is (c) 2008-2010 Plant Bioinformatics Group, IPK Gatersleben,
  * http://bioinformatics.ipk-gatersleben.de
- *
+ * 
  * The source code for this project which is developed by our group is available
- * under the GPL license v2.0 (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html).
- * By using this Add-on and VANTED you need to accept the terms and conditions of
- * this license, the below stated disclaimer of warranties and the licenses of the used
- * libraries. For further details see license.txt in the root folder of this project.
+ * under the GPL license v2.0
+ * (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html).
+ * By using this Add-on and VANTED you need to accept the terms and conditions
+ * of
+ * this license, the below stated disclaimer of warranties and the licenses of
+ * the used
+ * libraries. For further details see license.txt in the root folder of this
+ * project.
  ************************************************************************************/
 package de.ipk.ag_ba.rmi_server.analysis.image_analysis_tasks.reconstruction3d;
 
@@ -93,6 +97,8 @@ public class MyPicture {
 		else {
 			if (isTransparentPixel(cubeRelativePixel))
 				return null;
+			if (x + offX < 0 || y + offY < 0 || x + offX >= width || y + offY >= height)
+				return null;
 			int rgb = getRGB(x + offX, y + offY);
 			return new Color(rgb);
 		}
@@ -104,7 +110,7 @@ public class MyPicture {
 	}
 
 	public boolean setPictureData(BufferedImage bufferedImage, double angle, ModelGenerator mg, TransparencyAnalysis ta,
-			boolean isTop, double blurfactor) {
+						boolean isTop, double blurfactor) {
 
 		this.angle = angle;
 		this.cosAngle = Math.cos(angle);
