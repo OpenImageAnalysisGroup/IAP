f528683bd6e26cf7b20774e9897776090e41461a
klukas <klukas@90d7db82-dce4-4103-b106-19f43c5924aa>
2011-01-12 16:26:34 +0000

    *** empty log message ***

--- trunk/IAP/src/de/ipk/ag_ba/gui/navigation_model/RemoteExecutionWrapperAction.java
+++ trunk/IAP/src/de/ipk/ag_ba/gui/navigation_model/RemoteExecutionWrapperAction.java
@@ -42,7 +42,7 @@ public class RemoteExecutionWrapperAction implements NavigationAction {
 			TreeSet<Integer> jobIDs = new TreeSet<Integer>();
 			{
 				int idx = 0;
-				while (jobIDs.size() < 10)
+				while (jobIDs.size() < 50)
 					jobIDs.add(idx++);
 			}
 			for (int id : jobIDs) {
