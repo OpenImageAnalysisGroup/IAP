efdcdceba8e1e0d4e80cf2a8ffe002423f8be83a
klukas <klukas@90d7db82-dce4-4103-b106-19f43c5924aa>
2011-03-09 12:36:14 +0000

    *** empty log message ***

---
+++ trunk/IAP/src/de/ipk/ag_ba/gui/navigation_actions/LemnaTecLogoutAction.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Image Analysis Group, IPK Gatersleben
+ *******************************************************************************/
+/*
+ * Created on Oct 8, 2010 by Christian Klukas
+ */
+package de.ipk.ag_ba.gui.navigation_actions;
+
+import java.util.ArrayList;
+
+import de.ipk.ag_ba.gui.MainPanelComponent;
+import de.ipk.ag_ba.gui.interfaces.NavigationAction;
+import de.ipk.ag_ba.gui.navigation_model.NavigationButton;
+
+/**
+ * @author klukas
+ */
+public class LemnaTecLogoutAction extends AbstractNavigationAction implements NavigationAction {
+	
+	public LemnaTecLogoutAction() {
+		super("Logout");
+	}
+	
+	private NavigationButton src;
+	
+	@Override
+	public ArrayList<NavigationButton> getResultNewActionSet() {
+		ArrayList<NavigationButton> result = new ArrayList<NavigationButton>();
+		return result;
+	}
+	
+	@Override
+	public ArrayList<NavigationButton> getResultNewNavigationSet(ArrayList<NavigationButton> currentSet) {
+		ArrayList<NavigationButton> result = new ArrayList<NavigationButton>(currentSet);
+		result.add(src);
+		return result;
+	}
+	
+	@Override
+	public void performActionCalculateResults(NavigationButton src) throws Exception {
+		this.src = src;
+	}
+	
+	@Override
+	public MainPanelComponent getResultMainPanel() {
+		return new MainPanelComponent("This command has no function when using the applet version of IAP.");
+	}
+	
+	@Override
+	public String getDefaultImage() {
+		return "img/ext/logout.png";
+	}
+	
+	@Override
+	public String getDefaultNavigationImage() {
+		return "img/ext/logout.png";
+	}
+	
+	@Override
+	public String getDefaultTitle() {
+		return "Logout";
+	}
+	
+}
