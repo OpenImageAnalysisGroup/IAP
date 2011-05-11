b6418ce969ca1cb6754050488400e426a5a87505
entzian <entzian@90d7db82-dce4-4103-b106-19f43c5924aa>
2011-05-11 12:04:04 +0000

    *** empty log message ***

--- IAP/src/de/ipk/ag_ba/image/operations/blocks/cmds/BlockCountPixel.java
+++ IAP/src/de/ipk/ag_ba/image/operations/blocks/cmds/BlockCountPixel.java
@@ -3,10 +3,50 @@
  */
 package de.ipk.ag_ba.image.operations.blocks.cmds;
 
+import de.ipk.ag_ba.image.operations.blocks.cmds.data_structures.AbstractSnapshotAnalysisBlockFIS;
+import de.ipk.ag_ba.image.operations.blocks.properties.PropertyNames;
+import de.ipk.ag_ba.image.structures.FlexibleImage;
+
 /**
  * @author Entzian
- *
  */
-public class BlockCountPixel {
-
+public class BlockCountPixel extends AbstractSnapshotAnalysisBlockFIS {
+	
+	@Override
+	protected FlexibleImage processVISmask() {
+		
+		getProperties().setNumericProperty(0, PropertyNames.COUNT_PIXEL_VIS, countPixel(getInput().getMasks().getVis()));
+		
+		return getInput().getMasks().getVis();
+	}
+	
+	@Override
+	protected FlexibleImage processFLUOmask() {
+		
+		getProperties().setNumericProperty(0, PropertyNames.COUNT_PIXEL_FLUO, countPixel(getInput().getMasks().getFluo()));
+		
+		return getInput().getMasks().getFluo();
+	}
+	
+	@Override
+	protected FlexibleImage processNIRmask() {
+		
+		getProperties().setNumericProperty(0, PropertyNames.COUNT_PIXEL_NIR, countPixel(getInput().getMasks().getNir()));
+		
+		return getInput().getMasks().getNir();
+	}
+	
+	private int countPixel(FlexibleImage workMask) {
+		int count = 0;
+		int[][] workArray = workMask.getAs2A();
+		
+		for (int i = 0; i < workMask.getWidth(); i++) {
+			for (int j = 0; j < workMask.getHeight(); j++) {
+				if (workArray[i][j] != options.getBackground())
+					count++;
+			}
+		}
+		return count;
+	}
+	
 }
