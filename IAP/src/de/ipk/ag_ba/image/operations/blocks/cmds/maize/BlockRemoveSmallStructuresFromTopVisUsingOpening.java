930b9d9ba3179427772dfdddc9463bb400078063
pape <j-mp@users.noreply.github.com>
2011-06-08 16:20:12 +0000

    *** empty log message ***

--- IAP/src/de/ipk/ag_ba/image/operations/blocks/cmds/maize/BlockRemoveSmallStructuresFromTopVisUsingOpening.java
+++ IAP/src/de/ipk/ag_ba/image/operations/blocks/cmds/maize/BlockRemoveSmallStructuresFromTopVisUsingOpening.java
@@ -1,6 +1,7 @@
 package de.ipk.ag_ba.image.operations.blocks.cmds.maize;
 
 import de.ipk.ag_ba.image.analysis.gernally.ImageProcessorOptions.CameraTyp;
+import de.ipk.ag_ba.image.analysis.gernally.ImageProcessorOptions.Setting;
 import de.ipk.ag_ba.image.operations.ImageOperation;
 import de.ipk.ag_ba.image.operations.blocks.cmds.data_structures.AbstractSnapshotAnalysisBlockFIS;
 import de.ipk.ag_ba.image.structures.FlexibleImage;
@@ -10,7 +11,8 @@ public class BlockRemoveSmallStructuresFromTopVisUsingOpening extends AbstractSn
 	@Override
 	protected FlexibleImage processVISmask() {
 		if (options.getCameraTyp() == CameraTyp.TOP) {
-			FlexibleImage mask = new ImageOperation(getInput().getMasks().getVis()).opening(1).getImage();
+			FlexibleImage mask = new ImageOperation(getInput().getMasks().getVis()).opening(
+					(int) (1 * options.getDoubleSetting(Setting.SCALE_FACTOR_DECREASE_IMG_AND_MASK))).getImage();
 			return new ImageOperation(getInput().getMasks().getVis()).applyMask_ResizeMaskIfNeeded(mask, options.getBackground()).getImage();
 		} else
 			return getInput().getMasks().getVis();
