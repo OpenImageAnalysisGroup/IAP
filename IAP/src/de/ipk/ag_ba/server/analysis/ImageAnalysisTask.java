b26aa6ca47bb144173a93fcad953759c2933b9d0
Dr. Christian Klukas <cklukas@users.noreply.github.com>
2011-08-08 06:39:42 +0000

    *** empty log message ***

--- IAP/src/de/ipk/ag_ba/server/analysis/ImageAnalysisTask.java
+++ IAP/src/de/ipk/ag_ba/server/analysis/ImageAnalysisTask.java
@@ -6,6 +6,7 @@ import org.BackgroundTaskStatusProviderSupportingExternalCall;
 
 import de.ipk.ag_ba.mongo.MongoDB;
 import de.ipk_gatersleben.ag_nw.graffiti.plugins.gui.editing_tools.script_helper.NumericMeasurementInterface;
+import de.ipk_gatersleben.ag_pbi.mmd.experimentdata.Sample3D;
 
 /**
  * @author klukas
@@ -23,7 +24,7 @@ public interface ImageAnalysisTask {
 	
 	public ImageAnalysisType[] getOutputTypes();
 	
-	public void setInput(Collection<NumericMeasurementInterface> input, MongoDB m, int workOnSubset, int numberOfSubsets);
+	public void setInput(Collection<Sample3D> input, MongoDB m, int workOnSubset, int numberOfSubsets);
 	
 	public Collection<NumericMeasurementInterface> getOutput();
 }
