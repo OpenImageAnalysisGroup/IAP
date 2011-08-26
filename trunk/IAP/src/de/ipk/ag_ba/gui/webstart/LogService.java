a7f57fd78eb68edb68d34865a3ba41548fa3eff5
klukas <klukas@90d7db82-dce4-4103-b106-19f43c5924aa>
2011-08-26 08:07:30 +0000

    *** empty log message ***

--- trunk/IAP/src/de/ipk/ag_ba/gui/webstart/LogService.java
+++ trunk/IAP/src/de/ipk/ag_ba/gui/webstart/LogService.java
@@ -4,22 +4,41 @@ import java.util.Stack;
 
 import de.ipk.ag_ba.mongo.IAPservice;
 import de.ipk.ag_ba.mongo.MongoDB;
+import de.ipk.ag_ba.server.task_management.SystemAnalysisExt;
 
 public class LogService {
 	
 	private static boolean ba13reachable = IAPservice.isMongoReachable();
 	
-	public String getLatestNews(int n, String pre, String preLine, String lineBreak, String follow) {
+	public String getLatestNews(int n, String pre, final String preLine, String lineBreak, String follow) {
 		StringBuilder res = new StringBuilder();
 		if (!ba13reachable) {
 			System.out.println("INFO: MAIN CLOUD DATABASE NOT REACHABLE: COULD RETRIEVE LATEST NEWS");
 			return res.toString();
 		}
 		try {
-			Stack<String> news = new Stack<String>();
-			for (String item : MongoDB.getDefaultCloud().getNews(5)) {
-				news.push(preLine + item);
-			}
+			final Stack<String> news = new Stack<String>();
+			Runnable r = new Runnable() {
+				@Override
+				public void run() {
+					for (String item : MongoDB.getDefaultCloud().getNews(5)) {
+						news.push(preLine + item);
+					}
+				}
+			};
+			Thread t = new Thread(r, "Read MonogDB news");
+			t.start();
+			long start = System.currentTimeMillis();
+			do {
+				Thread.sleep(10);
+				long current = System.currentTimeMillis();
+				if (current - start > 1000) {
+					news.add(SystemAnalysisExt.getCurrentTime()
+							+ ": Could not access latest news (time-out). <b>&quot;Data Processing&quot; function may not work correctly at the moment.</b> (system message)");
+					t.interrupt();
+					break;
+				}
+			} while (t.isAlive());
 			while (!news.empty()) {
 				String item = news.pop();
 				res.append(item);
