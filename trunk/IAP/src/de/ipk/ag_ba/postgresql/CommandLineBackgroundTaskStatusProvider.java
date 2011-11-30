77ad07e722ecda3f134c406118b4296777c6d3c8
klukas <klukas@90d7db82-dce4-4103-b106-19f43c5924aa>
2011-11-30 11:23:29 +0000

    *** empty log message ***

--- trunk/IAP/src/de/ipk/ag_ba/postgresql/CommandLineBackgroundTaskStatusProvider.java
+++ trunk/IAP/src/de/ipk/ag_ba/postgresql/CommandLineBackgroundTaskStatusProvider.java
@@ -14,53 +14,55 @@ import de.ipk_gatersleben.ag_nw.graffiti.services.task.BackgroundTaskStatusProvi
  */
 public class CommandLineBackgroundTaskStatusProvider extends
 		BackgroundTaskStatusProviderSupportingExternalCallImpl {
-
+	
 	private final boolean enabled;
 	private final boolean showOnlyProgressBar;
-
+	
 	public CommandLineBackgroundTaskStatusProvider(String status1,
 			String status2) {
 		super(status1, status2);
 		this.enabled = true;
 		this.showOnlyProgressBar = false;
 	}
-
+	
 	public CommandLineBackgroundTaskStatusProvider(boolean enabled) {
 		super("", "");
 		this.enabled = enabled;
 		this.showOnlyProgressBar = false;
 	}
-
+	
 	public CommandLineBackgroundTaskStatusProvider(boolean enabled,
 			boolean showOnlyProgressBar) {
 		super("", "");
 		this.enabled = enabled;
 		this.showOnlyProgressBar = showOnlyProgressBar;
 	}
-
+	
 	@Override
 	public void setCurrentStatusText1(String status) {
 		super.setCurrentStatusText1(status);
 		if (enabled && !showOnlyProgressBar)
 			System.out.println(status);
-		else if (enabled)
-			showProgress();
+		else
+			if (enabled)
+				showProgress();
 	}
-
+	
 	@Override
 	public void setCurrentStatusText2(String status) {
 		super.setCurrentStatusText2(status);
 		if (enabled && !showOnlyProgressBar)
 			System.out.println(status);
-		else if (enabled)
-			showProgress();
+		else
+			if (enabled)
+				showProgress();
 	}
-
+	
 	long lastOutput = -1;
 	long lastProgress = -100;
 	long firstProgressFineTime = -1;
 	double firstProgressFineValue = -1;
-
+	
 	private synchronized void showProgress() {
 		if (System.currentTimeMillis() - lastOutput > 10 * 1000
 				&& lastProgress != getCurrentStatusValue()) {
@@ -80,16 +82,22 @@ public class CommandLineBackgroundTaskStatusProvider extends
 						- firstProgressFineValue;
 				double speed = progress / timeForProgress;
 				double remainingTime = (100 - progress) / speed;
+				double fullTime = 100d / speed;
 				long finishTime = (long) (System.currentTimeMillis() + remainingTime);
 				System.out.println(SystemAnalysisExt.getCurrentTime()
 						+ "> INFO: Estimated finish time (100%): "
 						+ SystemAnalysisExt.getCurrentTime(finishTime));
+				
+				String remain = SystemAnalysisExt.getWaitTime((long) fullTime);
+				System.out.println(SystemAnalysisExt.getCurrentTime()
+						+ "> INFO: Estimated overall run time: "
+						+ remain);
 			}
 			System.out
 					.println("****************************************************************************************");
 			lastOutput = System.currentTimeMillis();
 			lastProgress = getCurrentStatusValue();
-
+			
 			if (lastProgress > 0) {
 				if (firstProgressFineValue < 0) {
 					firstProgressFineValue = getCurrentStatusValueFine();
