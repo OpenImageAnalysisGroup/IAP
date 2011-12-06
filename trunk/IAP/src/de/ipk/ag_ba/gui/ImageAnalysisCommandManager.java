575afcacf3614e5e1ae4c69153b3827f222b0840
entzian <entzian@90d7db82-dce4-4103-b106-19f43c5924aa>
2011-12-06 10:07:49 +0000

    *** empty log message ***

--- trunk/IAP/src/de/ipk/ag_ba/gui/ImageAnalysisCommandManager.java
+++ trunk/IAP/src/de/ipk/ag_ba/gui/ImageAnalysisCommandManager.java
@@ -59,9 +59,11 @@ public class ImageAnalysisCommandManager {
 		if (SystemAnalysis.isHeadless())
 			actions.add(new NavigationButton(new ActionNumericDataReport(m, experimentReference), guiSetting));
 		// if (SystemAnalysis.isHeadless())
-		actions.add(new NavigationButton(new ActionNumericDataReportComplete(m, experimentReference, false, new String[] { "A" }), guiSetting));
-		actions.add(new NavigationButton(new ActionNumericDataReportComplete(m, experimentReference, false, new String[] { "B" }), guiSetting));
-		actions.add(new NavigationButton(new ActionNumericDataReportComplete(m, experimentReference, false, new String[] { "C" }), guiSetting));
+		actions.add(new NavigationButton(new ActionNumericDataReportComplete(m, experimentReference, false, new String[] { "Condition" }), guiSetting));
+		actions.add(new NavigationButton(new ActionNumericDataReportComplete(m, experimentReference, false, new String[] { "Treatment" }), guiSetting));
+		// actions.add(new NavigationButton(new ActionNumericDataReportComplete(m, experimentReference, false, new String[] { "Variety", "Treatment" }),
+		// guiSetting));
+		actions.add(new NavigationButton(new ActionNumericDataReportComplete(m, experimentReference, false, new String[] { "Species" }), guiSetting));
 		// actions.add(new NavigationButton(new ActionNumericDataReportComplete(m, experimentReference, true), guiSetting));
 		String hsmf = IAPmain.getHSMfolder();
 		if (hsmf != null)
