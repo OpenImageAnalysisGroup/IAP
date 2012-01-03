56b298eb77f273ac32f475677fb90f1db4cd21bf
Dr. Christian Klukas <cklukas@users.noreply.github.com>
2012-01-03 11:37:56 +0000

    *** empty log message ***

--- IAP/src/de/ipk/ag_ba/image/operations/blocks/cmds/BlockClearNirPot_nir.java
+++ IAP/src/de/ipk/ag_ba/image/operations/blocks/cmds/BlockClearNirPot_nir.java
@@ -17,7 +17,7 @@ public class BlockClearNirPot_nir extends AbstractSnapshotAnalysisBlockFIS {
 	protected FlexibleImage processNIRimage() {
 		FlexibleImage nir = getInput().getImages().getNir();
 		
-		if (nir == null || nir.getWidth() < 10 || nir.getHeight() < 10)
+		if (nir == null || nir.getWidth() < 10 || nir.getHeight() < 10 || options.isBarleyInBarleySystem())
 			return nir;
 		
 		nir.print("NIR pot analysis IN", debug);
@@ -27,42 +27,42 @@ public class BlockClearNirPot_nir extends AbstractSnapshotAnalysisBlockFIS {
 		int w = nir.getWidth();
 		int h = nir.getHeight();
 		
-		if (h>22)
-		for (int y = h - 1; y >= 21; y--) {
-			int maxContinousBlack = 0;
-			int currentlyBlack = 0;
-			int nSkipWhite = 0;
-			for (int x = 0; x < w; x++) {
-				int v = nirArray[x][y] & 0x0000ff;
-				int vAbove = nirArray[x][y - 20] & 0x0000ff;
-				if (v < 110 || nSkipWhite < 10 || vAbove < 110) {
-					currentlyBlack++;
-					if (v >= 100)
-						nSkipWhite++;
-				} else {
-					nSkipWhite = 0;
-					if (currentlyBlack > maxContinousBlack)
-						maxContinousBlack = currentlyBlack;
-					currentlyBlack = 0;
-				}
-			}
-			int g = 180;
-			int gray = (0xFF << 24 | (g & 0xFF) << 16) | ((g & 0xFF) << 8) | ((g & 0xFF) << 0);
-			if (maxContinousBlack > 0.1 * w) {
+		if (h > 22)
+			for (int y = h - 1; y >= 21; y--) {
+				int maxContinousBlack = 0;
+				int currentlyBlack = 0;
+				int nSkipWhite = 0;
 				for (int x = 0; x < w; x++) {
-					nirArray[x][y] = gray;
+					int v = nirArray[x][y] & 0x0000ff;
+					int vAbove = nirArray[x][y - 20] & 0x0000ff;
+					if (v < 110 || nSkipWhite < 10 || vAbove < 110) {
+						currentlyBlack++;
+						if (v >= 100)
+							nSkipWhite++;
+					} else {
+						nSkipWhite = 0;
+						if (currentlyBlack > maxContinousBlack)
+							maxContinousBlack = currentlyBlack;
+						currentlyBlack = 0;
+					}
 				}
-			} else {
-				int cut = y - 3;
-				for (y = cut + 3; y >= cut; y--) {
-					if (y > 0)
-						for (int x = 0; x < w; x++) {
-							nirArray[x][y] = gray;
-						}
+				int g = 180;
+				int gray = (0xFF << 24 | (g & 0xFF) << 16) | ((g & 0xFF) << 8) | ((g & 0xFF) << 0);
+				if (maxContinousBlack > 0.1 * w) {
+					for (int x = 0; x < w; x++) {
+						nirArray[x][y] = gray;
+					}
+				} else {
+					int cut = y - 3;
+					for (y = cut + 3; y >= cut; y--) {
+						if (y > 0)
+							for (int x = 0; x < w; x++) {
+								nirArray[x][y] = gray;
+							}
+					}
+					break;
 				}
-				break;
 			}
-		}
 		
 		return new FlexibleImage(nirArray).print("NIR pot removed", debug);
 	}
