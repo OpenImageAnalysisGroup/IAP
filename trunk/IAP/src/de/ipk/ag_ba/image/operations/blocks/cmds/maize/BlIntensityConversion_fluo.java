cb5a288f8048c7cecfb0e08a4823da4e41bb91cd
klukas <klukas@90d7db82-dce4-4103-b106-19f43c5924aa>
2012-01-04 14:19:20 +0000

    *** empty log message ***

--- trunk/IAP/src/de/ipk/ag_ba/image/operations/blocks/cmds/maize/BlIntensityConversion_fluo.java
+++ trunk/IAP/src/de/ipk/ag_ba/image/operations/blocks/cmds/maize/BlIntensityConversion_fluo.java
@@ -24,7 +24,7 @@ public class BlIntensityConversion_fluo extends AbstractSnapshotAnalysisBlockFIS
 		if (options.getCameraPosition() == CameraPosition.SIDE)
 			min = 210;
 		if (options.isBarleyInBarleySystem())
-			min = 190; // 225
+			min = options.getCameraPosition() == CameraPosition.SIDE ? 225 : 190;
 		FlexibleImage resChlorophyll = io.copy().convertFluo2intensity(FluoAnalysis.CHLOROPHYL, min).getImage();
 		FlexibleImage resPhenol = io.copy().convertFluo2intensity(FluoAnalysis.PHENOL, min).getImage();
 		FlexibleImage resClassic = io.convertFluo2intensity(FluoAnalysis.CLASSIC, min).getImage();
