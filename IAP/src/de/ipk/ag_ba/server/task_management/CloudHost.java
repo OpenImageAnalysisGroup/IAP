d2cc0c7a16f3c0423fdcf2d0ab9a03777af9d975
Dr. Christian Klukas <cklukas@users.noreply.github.com>
2012-01-09 16:33:40 +0000

    *** empty log message ***

--- IAP/src/de/ipk/ag_ba/server/task_management/CloudHost.java
+++ IAP/src/de/ipk/ag_ba/server/task_management/CloudHost.java
@@ -136,8 +136,8 @@ public class CloudHost extends BasicDBObject {
 	}
 	
 	public boolean isClusterExecutionMode() {
-		if (true)
-			return true;
+		// if (true)
+		// return true;
 		if (get("clusterMode") != null)
 			return (Boolean) get("clusterMode");
 		return false;
