58dd419b970f0faa08195f5de3bda5582a0d9f1d
klukas <klukas@90d7db82-dce4-4103-b106-19f43c5924aa>
2012-01-31 16:53:07 +0000

    *** empty log message ***

--- trunk/IAP/src/de/ipk/ag_ba/gui/webstart/IAPmain.java
+++ trunk/IAP/src/de/ipk/ag_ba/gui/webstart/IAPmain.java
@@ -382,7 +382,7 @@ public class IAPmain extends JApplet {
 			// see
 			switch (feature) {
 				case REMOTE_EXECUTION:
-					return false;
+					return true;
 				case SAVE_DEBUG_STACK:
 					return false;
 				case DELETE_CLOUD_JOBS_AND_TEMP_DATA_UPON_CLOUD_START:
@@ -394,7 +394,7 @@ public class IAPmain extends JApplet {
 			// these may be changed for interactive applet version !!!
 			switch (feature) {
 				case REMOTE_EXECUTION:
-					return false;
+					return true;
 				case SAVE_DEBUG_STACK:
 					return false;
 				case DELETE_CLOUD_JOBS_AND_TEMP_DATA_UPON_CLOUD_START:
