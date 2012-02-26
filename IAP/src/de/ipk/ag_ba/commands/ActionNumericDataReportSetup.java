bc68dc4879309ab7d653a5bab2b5cd0d4ebee121
Dr. Christian Klukas <cklukas@users.noreply.github.com>
2012-02-26 13:50:42 +0000

    *** empty log message ***

--- IAP/src/de/ipk/ag_ba/commands/ActionNumericDataReportSetup.java
+++ IAP/src/de/ipk/ag_ba/commands/ActionNumericDataReportSetup.java
@@ -218,7 +218,7 @@ public class ActionNumericDataReportSetup extends AbstractNavigationAction imple
 					tso.setParam(0, setting);
 					tso.setParam(1, c);
 					togglesForReport.add(tso);
-					settings.add(new NavigationButton(new ActionToggle("Include " + c + "?", c, tso), src.getGUIsetting()));
+					settings.add(new NavigationButton(new ActionToggle("Include " + setting + " " + c + "?", c, tso), src.getGUIsetting()));
 				}
 			if (setting.equalsIgnoreCase("Species"))
 				for (String c : ss) {
@@ -226,7 +226,7 @@ public class ActionNumericDataReportSetup extends AbstractNavigationAction imple
 					tso.setParam(0, setting);
 					tso.setParam(1, c);
 					togglesForReport.add(tso);
-					settings.add(new NavigationButton(new ActionToggle("Include " + c + "?", c, tso), src.getGUIsetting()));
+					settings.add(new NavigationButton(new ActionToggle("Include " + setting + " " + c + "?", c, tso), src.getGUIsetting()));
 				}
 			if (setting.equalsIgnoreCase("Genotype"))
 				for (String c : gs) {
@@ -234,7 +234,7 @@ public class ActionNumericDataReportSetup extends AbstractNavigationAction imple
 					tso.setParam(0, setting);
 					tso.setParam(1, c);
 					togglesForReport.add(tso);
-					settings.add(new NavigationButton(new ActionToggle("Include " + c + "?", c, tso), src.getGUIsetting()));
+					settings.add(new NavigationButton(new ActionToggle("Include " + setting + " " + c + "?", c, tso), src.getGUIsetting()));
 				}
 			if (setting.equalsIgnoreCase("Variety"))
 				for (String c : vs) {
@@ -242,7 +242,7 @@ public class ActionNumericDataReportSetup extends AbstractNavigationAction imple
 					tso.setParam(0, setting);
 					tso.setParam(1, c);
 					togglesForReport.add(tso);
-					settings.add(new NavigationButton(new ActionToggle("Include " + c + "?", c, tso), src.getGUIsetting()));
+					settings.add(new NavigationButton(new ActionToggle("Include " + setting + " " + c + "?", c, tso), src.getGUIsetting()));
 				}
 			if (setting.equalsIgnoreCase("Growth condition"))
 				for (String c : gc) {
@@ -250,7 +250,7 @@ public class ActionNumericDataReportSetup extends AbstractNavigationAction imple
 					tso.setParam(0, setting);
 					tso.setParam(1, c);
 					togglesForReport.add(tso);
-					settings.add(new NavigationButton(new ActionToggle("Include " + c + "?", c, tso), src.getGUIsetting()));
+					settings.add(new NavigationButton(new ActionToggle("Include " + setting + " " + c + "?", c, tso), src.getGUIsetting()));
 				}
 			if (setting.equalsIgnoreCase("Treatment"))
 				for (String c : ts) {
@@ -258,7 +258,7 @@ public class ActionNumericDataReportSetup extends AbstractNavigationAction imple
 					tso.setParam(0, setting);
 					tso.setParam(1, c);
 					togglesForReport.add(tso);
-					settings.add(new NavigationButton(new ActionToggle("Include " + c + "?", c, tso), src.getGUIsetting()));
+					settings.add(new NavigationButton(new ActionToggle("Include " + setting + " " + c + "?", c, tso), src.getGUIsetting()));
 				}
 		}
 		if (togglesForReport.size() > 0)
