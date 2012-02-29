9808026ddc0bdf1fc1ba6703698f1156e79a0131
Dr. Christian Klukas <cklukas@users.noreply.github.com>
2012-02-29 12:42:09 +0000

    *** empty log message ***

--- IAP/src/de/ipk/ag_ba/server/pdf_report/diagramIAP.cmd
+++ IAP/src/de/ipk/ag_ba/server/pdf_report/diagramIAP.cmd
@@ -1,32 +1,8 @@
 #!/bin/bash
 cd $(dirname $0)
 echo "Current directory: $(pwd)"
-PARAM=$#
-INPUTFILE="report.csv"
 
-if [ $PARAM = 1 ]; then
-	TREAT=${1}
-	SEC="none"
-	THI="FALSE"
-elif [ $PARAM = 2 ]; then
-	TREAT=${1}
-	SEC=${2}
-	THI="FALSE"	
-elif [ $PARAM = 3 ]; then
-	TREAT=${1}
-	SEC=${2}
-	THI=${3}
-else
-	TREAT="Genotype"
-	SEC="none"
-	THI="FALSE"
-fi
-
-if [ ${THI} = "TRUE" ]; then
-	Rscript createDiagrams.r ${INPUTFILE} pdf TRUE ${TREAT} ${SEC}
-else
-	Rscript createDiagrams.r ${INPUTFILE} pdf FALSE ${TREAT} ${SEC}
-fi
+Rscript createDiagrams.r report.csv pdf ${3} ${1} ${2}
 
 echo "Create PDF..."
 /usr/bin/pdflatex report.tex
