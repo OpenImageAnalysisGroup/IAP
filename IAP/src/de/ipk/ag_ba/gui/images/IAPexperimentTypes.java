1657e774dec734682bb957c0a1071ad909df05ae
Dr. Christian Klukas <cklukas@users.noreply.github.com>
2012-04-10 11:28:12 +0000

    *** empty log message ***

--- IAP/src/de/ipk/ag_ba/gui/images/IAPexperimentTypes.java
+++ IAP/src/de/ipk/ag_ba/gui/images/IAPexperimentTypes.java
@@ -1,21 +1,35 @@
 package de.ipk.ag_ba.gui.images;
 
 public enum IAPexperimentTypes {
-	Phytochamber("Arabidopsis"),
-	PhytochamberBlueRubber("Arabidopsis (blue rubber cover)"),
-	BarleyGreenhouse("Barley"),
-	MaizeGreenhouse("Maize"),
-	UnknownGreenhouse("LemnaTec (Other)"),
-	AnalysisResults("Analysis Results");
+	Phytochamber("Arabidopsis", "Arabidopsis"),
+	PhytochamberBlueRubber("Arabidopsis (blue rubber cover)", "Arabidopsis"),
+	BarleyGreenhouse("Barley", "Hordeum vulgare"),
+	MaizeGreenhouse("Maize", "Zea"),
+	UnknownGreenhouse("LemnaTec (Other)", "Unknown"),
+	AnalysisResults("Analysis Results", "Unknown"),
+	Raps("Rapeseed", "Canola");
 	
-	private String title;
+	private String title, species;
 	
-	IAPexperimentTypes(String title) {
+	IAPexperimentTypes(String title, String species) {
 		this.title = title;
+		this.species = species;
 	}
 	
 	@Override
 	public String toString() {
 		return title;
+	}
+	
+	public static String getSpeciesFromExperimentType(String expType) {
+		for (IAPexperimentTypes t : IAPexperimentTypes.values()) {
+			if (expType.equals(t.toString()))
+				return t.getSpecies();
+		}
+		return "Unknown";
+	}
+	
+	private String getSpecies() {
+		return species;
 	}
 }
