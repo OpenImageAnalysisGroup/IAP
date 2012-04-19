7bf106f1cf4e8f88a531b7388b07d01b3b444116
entzian <entzian@90d7db82-dce4-4103-b106-19f43c5924aa>
2012-04-19 12:54:29 +0000

    *** empty log message ***

--- IAP/src/de/ipk/ag_ba/server/pdf_report/diagramIAP.bat
+++ IAP/src/de/ipk/ag_ba/server/pdf_report/diagramIAP.bat
@@ -6,14 +6,16 @@ cd
 echo Condition 1: %1
 echo Condition 2: %2
 echo Appendix?  : %3
+echo Ratio?  : %4
 
-Rscript createDiagrams.r report.csv pdf %3 %1 %2
+Rscript createDiagrams.r report.csv pdf %3 %4 %1 %2
 
 rem The delete option is important at the development phase
 rem del /s report.aux
 rem del /s report.out
 rem del /s report.toc
 
+pdflatex report.tex
 pdflatex report.tex
 pdflatex report.tex
 
