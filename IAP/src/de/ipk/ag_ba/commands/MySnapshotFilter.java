33da93ca046943479e1d351f7248b5e7097c78a5
Dr. Christian Klukas <cklukas@users.noreply.github.com>
2012-05-11 14:48:20 +0000

    *** empty log message ***

--- IAP/src/de/ipk/ag_ba/commands/MySnapshotFilter.java
+++ IAP/src/de/ipk/ag_ba/commands/MySnapshotFilter.java
@@ -15,7 +15,7 @@ public class MySnapshotFilter implements SnapshotFilter {
 	
 	public MySnapshotFilter(ArrayList<ThreadSafeOptions> toggles, String globalOutlierList) {
 		this.toggles = toggles;
-		this.globalOutlierArr = globalOutlierList.split(",");
+		this.globalOutlierArr = globalOutlierList != null ? globalOutlierList.split(",") : new String[] {};
 		this.globalOutlierDays = new int[globalOutlierArr.length];
 		int idx = 0;
 		for (String o : globalOutlierArr) {
