5e4809476975f124008f2eaf3c5d307dfdfe5b1e
klukas <klukas@90d7db82-dce4-4103-b106-19f43c5924aa>
2012-05-11 23:13:10 +0000

    *** empty log message ***

--- trunk/IAP/src/de/ipk/ag_ba/gui/webstart/IAPmain.java
+++ trunk/IAP/src/de/ipk/ag_ba/gui/webstart/IAPmain.java
@@ -60,7 +60,7 @@ import de.ipk_gatersleben.ag_nw.graffiti.services.task.BackgroundTaskStatusProvi
 public class IAPmain extends JApplet {
 	private static final long serialVersionUID = 1L;
 	
-	public static final String RELEASE_IAP_IMAGE_ANALYSIS = "R26_2012-05-08_blue_rubber_IR_analysis";
+	public static final String RELEASE_IAP_IMAGE_ANALYSIS = "R27_2012-05-11_barley_top_vis_and_nir_top_fixes";
 	// public static final String RELEASE_IAP_IMAGE_ANALYSIS = "R25_2012-04-27_multi_tray_barley_in_maize";
 	// public static final String RELEASE_IAP_IMAGE_ANALYSIS = "R24_2012-04-16_remove_not_needed_histograms";
 	// public static final String RELEASE_IAP_IMAGE_ANALYSIS = "R23_2012-04-14_relative_leaf_length";
