6b9bacf38d4ee2a581a2cb6169b01731098abeee
Dr. Christian Klukas <cklukas@users.noreply.github.com>
2012-05-11 06:36:29 +0000

    *** empty log message ***

--- IAP/src/de/ipk/ag_ba/commands/ActionNumericDataReportComplete.java
+++ IAP/src/de/ipk/ag_ba/commands/ActionNumericDataReportComplete.java
@@ -191,7 +191,7 @@ public class ActionNumericDataReportComplete extends AbstractNavigationAction im
 			if (ratio) {
 				if (status != null)
 					status.setCurrentStatusText2("Calculate stress-ratio");
-				experiment = experiment.calc().ratioDataset("norm", cf);
+				experiment = experiment.calc().ratioDataset(new String[] { "norm", "sufficient" }, cf);
 				if (status != null)
 					status.setCurrentStatusText2("Calculate 3-segment linear model");
 				experiment.calc().fitThreeStepLinearModel("side.hull.pc2.norm");
