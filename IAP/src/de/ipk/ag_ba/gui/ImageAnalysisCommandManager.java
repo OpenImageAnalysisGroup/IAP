d2b635e5202788097b388402c8a6172b4375be3c
Dr. Christian Klukas <cklukas@users.noreply.github.com>
2012-06-28 10:52:19 +0000

    *** empty log message ***

--- IAP/src/de/ipk/ag_ba/gui/ImageAnalysisCommandManager.java
+++ IAP/src/de/ipk/ag_ba/gui/ImageAnalysisCommandManager.java
@@ -260,12 +260,20 @@ public class ImageAnalysisCommandManager {
 	
 	protected static String getList(String heading, TreeSet<String> cs) {
 		StringBuilder res = new StringBuilder();
-		res.append(heading + "<ul>");
+		res.append(heading + " (" + cs.size() + ")" + "<ul>");
 		if (cs.size() == 0)
 			res.append("<li>[NOT SPECIFIED]");
-		else
-			for (String c : cs)
+		else {
+			int n = 0;
+			for (String c : cs) {
 				res.append("<li>" + StringManipulationTools.stringReplace(c, ";", ";<br>"));
+				n++;
+				if (cs.size() > 15 && n >= 15) {
+					res.append("<li>...");
+					break;
+				}
+			}
+		}
 		res.append("</ul>");
 		return res.toString();
 	}
