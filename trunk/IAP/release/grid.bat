60d78dd7cee9a2b21526f3ef0363c8a5746805da
klukas <klukas@90d7db82-dce4-4103-b106-19f43c5924aa>
2012-07-27 05:37:32 +0000

    *** empty log message ***

--- trunk/IAP/release/grid.bat
+++ trunk/IAP/release/grid.bat
@@ -7,7 +7,10 @@ echo >Current path:
 cd
 :download
 echo >Current client will be downloaded from http://ba-13.ipk-gatersleben.de/iap.jar
-echo java -Xmx1g -cp iap.jar iap.download http://ba-13.ipk-gatersleben.de/iap.jar
+java -cp iap.jar iap.Download http://ba-13.ipk-gatersleben.de/iap.jar iap2.jar
+IF %ERRORLEVEL% NEQ 0 GOTO downloaderror 
+echo Move downloaded jar into place ...
+move /Y iap2.jar iap.jar
 IF %ERRORLEVEL% NEQ 0 GOTO downloaderror 
 echo >JAR Release Info:
 dir iap.jar
@@ -16,14 +19,14 @@ echo >One moment, start grid job execution ...
 java -Xmx12g -jar iap.jar close
 echo >Grid client closed
 IF %ERRORLEVEL% NEQ 0 GOTO morework 
-echo >Return value indicates no more work needs to be done. Waiting 5 minutes for restart.
-PING 1.1.1.1 -n 5 -w 60000 2>NUL
+echo >Return value indicates no more work needs to be done. Waiting 1 minute for restart.
+PING 1.1.1.1 -n 1 -w 60000 >NUL
 GOTO download
 :morework
-echo >Return value indicates more work needs to be done. Start processing next job. One moment ...
-PING 1.1.1.1 -n 1 -w 10000 2>NUL
+echo >Return value indicates more work needs to be done. Start processing next job in 10 seconds ...
+PING 1.1.1.1 -n 1 -w 10000 >NUL
 GOTO start
 :downloaderror
 echo >Return value indicates that the current release could not be downloaded. Waiting 10 minutes for next try.
-PING 1.1.1.1 -n 10 -w 60000 2>NUL
+PING 1.1.1.1 -n 10 -w 60000 >NUL
 GOTO download
