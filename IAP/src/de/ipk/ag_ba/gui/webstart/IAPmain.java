469cd38fa8859f2aa31aacdbc3ac09aab3eee70a
Dr. Christian Klukas <cklukas@users.noreply.github.com>
2012-07-31 13:25:00 +0000

    *** empty log message ***

--- IAP/src/de/ipk/ag_ba/gui/webstart/IAPmain.java
+++ IAP/src/de/ipk/ag_ba/gui/webstart/IAPmain.java
@@ -386,7 +386,7 @@ public class IAPmain extends JApplet {
 				case DELETE_CLOUD_JOBS_AND_TEMP_DATA_UPON_CLOUD_START:
 					return false;
 				case TOMCAT_AUTOMATIC_HSM_BACKUP:
-					return true;
+					return false;
 			}
 		} else {
 			// these may be changed for interactive applet version !!!
@@ -394,6 +394,8 @@ public class IAPmain extends JApplet {
 				case REMOTE_EXECUTION:
 					return true;
 				case SAVE_DEBUG_STACK:
+					return false;
+				case TOMCAT_AUTOMATIC_HSM_BACKUP:
 					return false;
 				case DELETE_CLOUD_JOBS_AND_TEMP_DATA_UPON_CLOUD_START:
 					return false;
