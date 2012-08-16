e2fec50a7521c741fd7a724a16f2b45a03bac262
entzian <entzian@90d7db82-dce4-4103-b106-19f43c5924aa>
2012-08-16 06:12:36 +0000

    *** empty log message ***

--- IAP/src/de/ipk/ag_ba/server/pdf_report/diagramIAP.cmd
+++ IAP/src/de/ipk/ag_ba/server/pdf_report/diagramIAP.cmd
@@ -2,14 +2,35 @@
 cd $(dirname $0)
 echo "Current directory: $(pwd)"
 
-if -a report.clustering.csv; then Rscript calcClusters.R ${6}; fi
+echo "#####"
+echo "Condition 1   : $1"
+echo "Condition 2   : $2"
+echo "Appendix?     : $3"
+echo "Ratio?        : $4"
+echo "Clustering?   : $5"
+echo "Bootstrap-n?  : $6"
+echo "Stress start? : $7"
+echo "Stress end?   : $8"
+echo "Stress typ?   : $9"
+echo "Stress label? : $10"
+echo "Split Cond1?  : $11"
+echo "Split Cond2?  : $12"
 
-Rscript createDiagrams.R report.csv pdf ${3} ${4} ${1} ${2} ${5}
+if [ -f report.clustering.csv ]
+then
+Rscript calcClusters.R $6
+fi
+
+Rscript createDiagrams.R report.csv pdf "$@"
 
 echo "Create PDF..."
-/usr/bin/pdflatex report.tex
-/usr/texbin/pdflatex report.tex
-/usr/bin/pdflatex report.tex
-/usr/texbin/pdflatex report.tex
+/usr/bin/pdflatex report.tex -interaction batchmode
+/usr/texbin/pdflatex report.tex -interaction batchmode
+
+/usr/bin/pdflatex report.tex -interaction batchmode
+/usr/texbin/pdflatex report.tex -interaction batchmode
+
+/usr/bin/pdflatex report.tex -interaction batchmode
+/usr/texbin/pdflatex report.tex -interaction batchmode
 echo ""
 echo "Finished"
