1a286bbf03738db90f041f382e5a37fa6349fb1f
klukas <klukas@90d7db82-dce4-4103-b106-19f43c5924aa>
2012-08-25 15:46:50 +0000

    *** empty log message ***

--- trunk/IAP/src/de/ipk/ag_ba/mongo/RunnableProcessingDBid.java
+++ trunk/IAP/src/de/ipk/ag_ba/mongo/RunnableProcessingDBid.java
@@ -1,5 +1,5 @@
 package de.ipk.ag_ba.mongo;
 
-public interface RunnableProcessingDBid extends Runnable {
-	public void setDBid(String id);
+public interface RunnableProcessingDBid {
+	public void processDBid(String id);
 }
