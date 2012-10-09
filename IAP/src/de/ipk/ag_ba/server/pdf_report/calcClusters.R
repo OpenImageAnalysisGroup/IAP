96b02bd3c72bc51cd1c1f387139645ace3d045de
Dr. Christian Klukas <cklukas@users.noreply.github.com>
2012-10-09 05:02:42 +0000

    *** empty log message ***

--- IAP/src/de/ipk/ag_ba/server/pdf_report/calcClusters.R
+++ IAP/src/de/ipk/ag_ba/server/pdf_report/calcClusters.R
@@ -6,6 +6,7 @@
 library(pvclust)
 mydata <- read.csv('report.clustering.csv', sep=';', row.names="UniID", header=TRUE)
 commandArgs(TRUE)[1] -> n
+n <- as.numeric(n)
 cat(c("Calculate clustering for",(length(mydata)),"groups. Using bootstrap N =",n,"...\n"))
 
 result <- NULL
@@ -21,14 +22,14 @@ if (n>0) {
 #		} )
 	
 
-	pdf("clusters.pdf")
+	pdf("clusters.pdf", width=15, height=15)
 #[2:length(mydata)]
 	plot(result)
 	pvrect(result, alpha=0.95)
 	dev.off()
 } else {
 	result <- hclust(d=dist(t(mydata)), method="ward")
-	pdf("clusters.pdf")
+	pdf("clusters.pdf", width=15, height=15)
 	plot(result)
 	dev.off()
 }
