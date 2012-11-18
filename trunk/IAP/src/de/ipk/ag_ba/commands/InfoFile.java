027ad95999e0cd564387f54a20c266366eee2ee6
klukas <klukas@90d7db82-dce4-4103-b106-19f43c5924aa>
2012-11-18 18:26:05 +0000

    *** empty log message ***

---
+++ trunk/IAP/src/de/ipk/ag_ba/commands/InfoFile.java
@@ -0,0 +1,10 @@
+package de.ipk.ag_ba.commands;
+
+import de.ipk_gatersleben.ag_nw.graffiti.plugins.gui.webstart.TextFile;
+
+public class InfoFile {
+	
+	public InfoFile(TextFile textFile) {
+	}
+	
+}
