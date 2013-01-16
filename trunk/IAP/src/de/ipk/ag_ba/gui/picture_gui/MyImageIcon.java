6840dca333cc00f38db2278836bfaab62c92d2fb
klukas <klukas@90d7db82-dce4-4103-b106-19f43c5924aa>
2013-01-16 18:15:19 +0000

    *** empty log message ***

--- trunk/IAP/src/de/ipk/ag_ba/gui/picture_gui/MyImageIcon.java
+++ trunk/IAP/src/de/ipk/ag_ba/gui/picture_gui/MyImageIcon.java
@@ -64,13 +64,17 @@ public class MyImageIcon extends ImageIcon {
 			BufferedImage i = null;
 			// if (width == 128 && height == 128) {
 			ResourceIOHandler ioh = ResourceIOManager.getHandlerFromPrefix(fileURLmain.getPrefix());
-			i = ImageIO.read(ioh.getPreviewInputStream(fileURLmain, width));
-			// } else {
-			// i = ImageIO.read(fileURLmain.getInputStream());
-			// int maxS = i.getHeight() > i.getWidth() ? i.getHeight() : i.getWidth();
-			// double factor = DataSetFileButton.ICON_HEIGHT / (double) maxS;
-			// i = resize(i, (int) (i.getWidth() * factor), (int) (i.getHeight() * factor));
-			// }
+			try {
+				i = ImageIO.read(ioh.getPreviewInputStream(fileURLmain, width));
+			} catch (Exception e) {
+				//
+			}
+			if (i == null) {
+				i = ImageIO.read(fileURLmain.getInputStream());
+				int maxS = i.getHeight() > i.getWidth() ? i.getHeight() : i.getWidth();
+				double factor = DataSetFileButton.ICON_HEIGHT / (double) maxS;
+				i = resize(i, (int) (i.getWidth() * factor), (int) (i.getHeight() * factor));
+			}
 			imageAvailable = 1;
 			setImage(i);
 			
