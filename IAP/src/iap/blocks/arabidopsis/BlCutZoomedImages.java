c61a17fdf127f3e49505c5269ef7b1d2d9b50503
Dr. Christian Klukas <cklukas@users.noreply.github.com>
2013-02-14 09:27:46 +0000

    *** empty log message ***

--- IAP/src/iap/blocks/arabidopsis/BlCutZoomedImages.java
+++ IAP/src/iap/blocks/arabidopsis/BlCutZoomedImages.java
@@ -78,6 +78,7 @@ public class BlCutZoomedImages extends AbstractBlock {
 					FlexibleImageSet in = ab.images();
 					
 					FlexibleImage vis = in.getImage(inpImageType).copy();
+					
 					int vs = jsp.getVerticalScrollBar().getValue();
 					int hs = jsp.getHorizontalScrollBar().getValue();
 					FlexibleImage selImage = ab.images().getImage(inpImageType);
