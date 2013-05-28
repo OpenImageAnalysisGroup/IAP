0de1cb432e3aecf4be77301a92603be1332eeda3
klukas <klukas@90d7db82-dce4-4103-b106-19f43c5924aa>
2013-05-28 10:18:31 +0000

    *** empty log message ***

--- trunk/IAP/src/de/ipk/ag_ba/mongo/MongoDB.java
+++ trunk/IAP/src/de/ipk/ag_ba/mongo/MongoDB.java
@@ -240,12 +240,12 @@ public class MongoDB {
 					if (m.get(key) == null) {
 						Builder mob = new MongoClientOptions.Builder();
 						mob.connectionsPerHost(SystemOptions.getInstance().getInteger("GRID-STORAGE", "connections per host", 1));
-						mob.connectTimeout(SystemOptions.getInstance().getInteger("GRID-STORAGE", "connect timeout", 24 * 60 * 60 * 1000));
+						mob.connectTimeout(SystemOptions.getInstance().getInteger("GRID-STORAGE", "connect timeout", 1 * 60 * 1000));
 						mob.threadsAllowedToBlockForConnectionMultiplier(SystemOptions.getInstance().getInteger("GRID-STORAGE",
 								"threads allowed to wait for connection",
 								1000000));
-						mob.connectTimeout(SystemOptions.getInstance().getInteger("GRID-STORAGE", "socket timeout", 24 * 60 * 60 * 1000));
-						mob.maxWaitTime(SystemOptions.getInstance().getInteger("GRID-STORAGE", "wax wait time", 24 * 60 * 60 * 1000));
+						mob.connectTimeout(SystemOptions.getInstance().getInteger("GRID-STORAGE", "socket timeout", 5 * 60 * 1000));
+						mob.maxWaitTime(SystemOptions.getInstance().getInteger("GRID-STORAGE", "max wait time", 5 * 60 * 1000));
 						mob.autoConnectRetry(SystemOptions.getInstance().getBoolean("GRID-STORAGE", "auto connect retry", true));
 						mob.writeConcern(WriteConcern.ACKNOWLEDGED);
 						
@@ -262,13 +262,13 @@ public class MongoDB {
 							m.put(key, new MongoClient(seeds, mco));
 						}
 						m.get(key).getMongoOptions().connectionsPerHost = SystemOptions.getInstance().getInteger("GRID-STORAGE", "connections per host", 1);
-						m.get(key).getMongoOptions().connectTimeout = SystemOptions.getInstance().getInteger("GRID-STORAGE", "connect timeout", 24 * 60 * 60 * 1000);
-						m.get(key).getMongoOptions().maxWaitTime = SystemOptions.getInstance().getInteger("GRID-STORAGE", "max wait time", 24 * 60 * 60 * 1000);
+						m.get(key).getMongoOptions().connectTimeout = SystemOptions.getInstance().getInteger("GRID-STORAGE", "connect timeout", 1 * 60 * 1000);
+						m.get(key).getMongoOptions().maxWaitTime = SystemOptions.getInstance().getInteger("GRID-STORAGE", "max wait time", 5 * 60 * 1000);
 						m.get(key).getMongoOptions().autoConnectRetry = SystemOptions.getInstance().getBoolean("GRID-STORAGE", "auto connect retry", true);
 						m.get(key).getMongoOptions().threadsAllowedToBlockForConnectionMultiplier = SystemOptions.getInstance().getInteger("GRID-STORAGE",
 								"threads allowed to wait for connection",
 								1000000);
-						m.get(key).getMongoOptions().connectTimeout = SystemOptions.getInstance().getInteger("GRID-STORAGE", "socket timeout", 24 * 60 * 60 * 1000);
+						m.get(key).getMongoOptions().connectTimeout = SystemOptions.getInstance().getInteger("GRID-STORAGE", "socket timeout", 5 * 60 * 1000);
 						s.printTime(1000);
 						if (authenticatedDBs.get(m.get(key)) == null || !authenticatedDBs.get(m.get(key)).contains("admin")) {
 							DB dbAdmin = m.get(key).getDB("admin");
