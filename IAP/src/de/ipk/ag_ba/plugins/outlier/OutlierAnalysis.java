d4e201cf3a7ec66ea9847dc400f0e54f2bd4213e
Dr. Christian Klukas <cklukas@users.noreply.github.com>
2013-06-06 14:03:46 +0000

    *** empty log message ***

--- IAP/src/de/ipk/ag_ba/plugins/outlier/OutlierAnalysis.java
+++ IAP/src/de/ipk/ag_ba/plugins/outlier/OutlierAnalysis.java
@@ -1,13 +1,14 @@
 package de.ipk.ag_ba.plugins.outlier;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.TreeMap;
 
 import org.ErrorMsg;
-import org.SystemAnalysis;
-import org.apache.commons.math3.stat.inference.TTest;
+import org.apache.commons.math3.distribution.TDistribution;
+import org.graffiti.plugin.io.resources.IOurl;
 
 import de.ipk.ag_ba.commands.AbstractNavigationAction;
 import de.ipk.ag_ba.commands.experiment.view_or_export.ActionDataProcessing;
@@ -15,6 +16,7 @@ import de.ipk.ag_ba.gui.MainPanelComponent;
 import de.ipk.ag_ba.gui.navigation_actions.ParameterOptions;
 import de.ipk.ag_ba.gui.navigation_model.NavigationButton;
 import de.ipk.ag_ba.gui.util.ExperimentReference;
+import de.ipk.ag_ba.plugins.outlier.data.Handler;
 import de.ipk_gatersleben.ag_nw.graffiti.plugins.gui.editing_tools.script_helper.ConditionInterface;
 import de.ipk_gatersleben.ag_nw.graffiti.plugins.gui.editing_tools.script_helper.Experiment;
 import de.ipk_gatersleben.ag_nw.graffiti.plugins.gui.editing_tools.script_helper.ExperimentInterface;
@@ -22,16 +24,20 @@ import de.ipk_gatersleben.ag_nw.graffiti.plugins.gui.editing_tools.script_helper
 import de.ipk_gatersleben.ag_nw.graffiti.plugins.gui.editing_tools.script_helper.NumericMeasurementInterface;
 import de.ipk_gatersleben.ag_nw.graffiti.plugins.gui.editing_tools.script_helper.SampleInterface;
 import de.ipk_gatersleben.ag_nw.graffiti.plugins.gui.editing_tools.script_helper.SubstanceInterface;
+import de.ipk_gatersleben.ag_pbi.mmd.experimentdata.images.ImageData;
 
 public class OutlierAnalysis extends AbstractNavigationAction implements ActionDataProcessing {
 	
 	private ExperimentReference experimentReference;
 	private String selTimepoint;
-	private double threshold = 0.00010;
+	private double threshold = 0.01;
 	protected ArrayList<String> calculationResults = new ArrayList<String>();
 	private int plantIDcount;
 	private int traitCount;
 	private int maxDescCount;
+	
+	private final HashMap<String, IOurl> visTopPlantID2url = new HashMap<String, IOurl>();
+	private final HashMap<String, IOurl> visSidePlantID2url = new HashMap<String, IOurl>();
 	
 	public OutlierAnalysis(String tooltip) {
 		super(tooltip);
@@ -77,6 +83,8 @@ public class OutlierAnalysis extends AbstractNavigationAction implements ActionD
 		ArrayList<Double> numericValues = new ArrayList<Double>();
 		ArrayList<String> plantIDs = new ArrayList<String>();
 		HashSet<String> allPlantIDs = new HashSet<String>();
+		visTopPlantID2url.clear();
+		visSidePlantID2url.clear();
 		maxDescCount = 0;
 		if (selTimepoint != null) {
 			ExperimentInterface exp = experimentReference.getData(status);
@@ -84,7 +92,30 @@ public class OutlierAnalysis extends AbstractNavigationAction implements ActionD
 			int idx = 0;
 			for (SubstanceInterface si : exp) {
 				String trait = si.getName();
+				if (trait.equals("side.vis") || trait.equals("top.vis") || trait.equals("vis.side") || trait.equals("vis.top"))
+					for (ConditionInterface ci : si) {
+						for (SampleInterface sai : ci) {
+							if (sai.getSampleTime().equals(selTimepoint)) {
+								for (NumericMeasurementInterface nmi : sai) {
+									if (nmi instanceof ImageData) {
+										ImageData id = (ImageData) nmi;
+										if (trait.contains("side"))
+											if (!visSidePlantID2url.containsKey(nmi.getQualityAnnotation()))
+												visSidePlantID2url.put(nmi.getQualityAnnotation(), id.getURL());
+										if (trait.contains("top"))
+											if (!visTopPlantID2url.containsKey(nmi.getQualityAnnotation()))
+												visTopPlantID2url.put(nmi.getQualityAnnotation(), id.getURL());
+									}
+								}
+							}
+						}
+					}
+			}
+			for (SubstanceInterface si : exp) {
+				String trait = si.getName();
 				if (trait.endsWith(".norm"))
+					continue;
+				if (trait.endsWith(".sum"))
 					continue;
 				if (trait.startsWith("RESULT_"))
 					continue;
@@ -92,7 +123,15 @@ public class OutlierAnalysis extends AbstractNavigationAction implements ActionD
 					continue;
 				if (trait.contains("section"))
 					continue;
+				if (trait.contains(".filled."))
+					continue;
 				if (trait.contains("histogram"))
+					continue;
+				if (trait.endsWith("_before"))
+					continue;
+				if (trait.endsWith("_sum"))
+					continue;
+				if (trait.contains("water"))
 					continue;
 				if (trait.contains("curling.n"))
 					continue;
@@ -103,56 +142,32 @@ public class OutlierAnalysis extends AbstractNavigationAction implements ActionD
 					status.setCurrentStatusText1("Process trait");
 					status.setCurrentStatusText2(trait);
 				}
+				HashMap<String, ArrayList<Double>> plantId2values = new HashMap<String, ArrayList<Double>>();
 				for (ConditionInterface ci : si) {
 					for (SampleInterface sai : ci) {
 						if (sai.getSampleTime().equals(selTimepoint)) {
-							numericValues.clear();
-							plantIDs.clear();
 							for (NumericMeasurementInterface nmi : sai) {
 								if (nmi instanceof NumericMeasurement) {
 									double val = nmi.getValue();
 									if (!Double.isNaN(val) && !Double.isInfinite(val) && nmi.getQualityAnnotation() != null && !nmi.getQualityAnnotation().isEmpty()) {
-										numericValues.add(val);
-										plantIDs.add(nmi.getQualityAnnotation());
-									}
-								}
-							}
-							if (plantIDs.size() > 3) {
-								int i = 0;
-								for (String p : plantIDs) {
-									allPlantIDs.add(p);
-									double val = numericValues.get(i);
-									double[] otherVals = new double[plantIDs.size() - 1];
-									int oi = 0, filled = 0;
-									for (Double v : numericValues) {
-										if (oi != i) {
-											otherVals[filled++] = v;
-										}
-										oi++;
-									}
-									i++;
-									double sum = 0;
-									for (double v : otherVals) {
-										sum += v;
-									}
-									try {
-										double mean = sum / otherVals.length;
-										double p_val = new TTest().tTest(val, otherVals);
-										if (p_val < threshold) {
-											if (!plantid2property2topOrLowPercentile.containsKey(p))
-												plantid2property2topOrLowPercentile.put(p, new LinkedHashMap<String, Boolean>());
-											plantid2property2topOrLowPercentile.get(p).put(si.getName(), val > mean);
-											if (plantid2property2topOrLowPercentile.get(p).size() > maxDescCount)
-												maxDescCount = plantid2property2topOrLowPercentile.get(p).size();
-										}
-									} catch (Exception e) {
-										System.out.println(SystemAnalysis.getCurrentTime() + ">ERROR: T-test calculation problem: " + e.getMessage());
+										if (!plantId2values.containsKey(nmi.getQualityAnnotation()))
+											plantId2values.put(nmi.getQualityAnnotation(), new ArrayList<Double>());
+										plantId2values.get(nmi.getQualityAnnotation()).add(val);
 									}
 								}
 							}
 						}
 					}
 				}
+				numericValues.clear();
+				plantIDs.clear();
+				for (String plantID : plantId2values.keySet()) {
+					numericValues.add(median(plantId2values.get(plantID)));
+					plantIDs.add(plantID);
+				}
+				
+				grubbsTest(plantIDs, trait, numericValues, plantid2property2topOrLowPercentile, threshold);
+				
 			}
 			processResults(plantid2property2topOrLowPercentile);
 			
@@ -166,24 +181,124 @@ public class OutlierAnalysis extends AbstractNavigationAction implements ActionD
 		plantIDcount = allPlantIDs.size();
 	}
 	
+	private Double median(ArrayList<Double> m) {
+		java.util.Collections.sort(m);
+		int middle = m.size() / 2;
+		if (m.size() % 2 == 1) {
+			return m.get(middle);
+		} else {
+			return (m.get(middle - 1) + m.get(middle)) / 2.0;
+		}
+	}
+	
+	/**
+	 * @return Number of removed outliers.
+	 */
+	private int grubbsTest(ArrayList<String> plantIDs, String trait, ArrayList<Double> numericValues,
+			TreeMap<String, LinkedHashMap<String, Boolean>> plantid2property2topOrLowPercentile, double alpha) {
+		boolean outlierIdentified;
+		int removedPoints = 0;
+		do {
+			outlierIdentified = false;
+			// G = (max {|Yi - Yavg|} ) / s
+			// Yavg = Average value
+			// s = StdDev
+			// calculate s (StdDev)
+			double sum = 0d;
+			double min = Double.MAX_VALUE;
+			double max = Double.NEGATIVE_INFINITY;
+			int indexOfMinValue = -1;
+			int indexOfMaxValue = -1;
+			int n = 0;
+			for (Double value : numericValues) {
+				sum += value;
+				if (value < min) {
+					min = value;
+					indexOfMinValue = n;
+				}
+				if (value > max) {
+					max = value;
+					indexOfMaxValue = n;
+				}
+				n++;
+			}
+			double avg = sum / n;
+			double sumDiff = 0;
+			for (Double value : numericValues) {
+				sumDiff += (value - avg) * (value - avg);
+			}
+			double stdDev = Math.sqrt(sumDiff / (n - 1));
+			double m1 = Math.abs(max - avg);
+			double m2 = Math.abs(min - avg);
+			double maxYi_Yavg = (m1 > m2 ? m1 : m2);
+			boolean isMaxPotentialOutlier = m1 > m2;
+			double G = maxYi_Yavg / stdDev;
+			// critical region (from Engineering Statistics Handbook)
+			// G > (N-1) / N^.5 * ( (t(a/(2N), N-2))^2
+			// /
+			// (N-2+(t(a/(2N),N-2))^2)
+			// )^0.5
+			if (n - 2 > 0) {
+				try {
+					TDistribution td = new TDistribution(n - 2);
+					double t1 = td.inverseCumulativeProbability(1 - (1 - alpha) / (2 * n));
+					double testG = (n - 1) / Math.sqrt(n) * Math.sqrt(t1 * t1 / (n - 2 + t1));
+					if (G > testG) {
+						String plantID;
+						if (isMaxPotentialOutlier) {
+							plantID = plantIDs.get(indexOfMaxValue);
+							plantIDs.remove(indexOfMaxValue);
+							numericValues.remove(indexOfMaxValue);
+						} else {
+							plantID = plantIDs.get(indexOfMinValue);
+							plantIDs.remove(indexOfMinValue);
+							numericValues.remove(indexOfMinValue);
+						}
+						if (!plantid2property2topOrLowPercentile.containsKey(plantID))
+							plantid2property2topOrLowPercentile.put(plantID, new LinkedHashMap<String, Boolean>());
+						plantid2property2topOrLowPercentile.get(plantID).put(trait, isMaxPotentialOutlier);
+						removedPoints++;
+						outlierIdentified = true;
+					}
+				} catch (Exception e) {
+					ErrorMsg.addErrorMessage(e);
+				}
+			}
+		} while (outlierIdentified);
+		return removedPoints;
+	}
+	
 	private void processResults(TreeMap<String, LinkedHashMap<String, Boolean>> plantid2property2topOrLowPercentile) {
 		StringBuilder sb = new StringBuilder();
-		sb.append("<html><table>");
-		sb.append("<tr><th colspan='" + (maxDescCount + 1) + "'>Processed " + traitCount + " traits and " + plantIDcount + " plant IDs</th></tr>");
-		sb.append("<tr><th>" + plantid2property2topOrLowPercentile.size() + " Plants</td><th colspan='" + maxDescCount + "'>Traits</th></tr>");
-		for (String p : plantid2property2topOrLowPercentile.keySet()) {
-			sb.append("<tr><td>" + p + "</td>");
-			LinkedHashMap<String, Boolean> traitChange = plantid2property2topOrLowPercentile.get(p);
-			for (String trait : traitChange.keySet()) {
-				if (traitChange.get(trait))
-					sb.append("<td bgcolor='#FFEEEE'>high " + trait + "</td>");
+		if (plantid2property2topOrLowPercentile.size() > 0) {
+			sb.append("<html><table>");
+			sb.append("<tr><th colspan='" + (maxDescCount + 2) + "'>Processed " + traitCount + " traits and found " + plantIDcount
+					+ " plant IDs with outlier trait values</th></tr>");
+			sb.append("<tr><th>" + plantid2property2topOrLowPercentile.size() + " Plants</td><th>Side View</th><th>Top View</th><th colspan='" + maxDescCount
+					+ "'>Traits</th></tr>");
+			for (String p : plantid2property2topOrLowPercentile.keySet()) {
+				sb.append("<tr><td>" + p + "</td>");
+				if (visSidePlantID2url.containsKey(p))
+					sb.append("<td><img src='data:" + visSidePlantID2url.get(p).toString() + "'/></td>");
 				else
-					sb.append("<td bgcolor='#EEEEFF'>low " + trait + "</td>");
+					sb.append("<td><td/>");
+				if (visTopPlantID2url.containsKey(p))
+					sb.append("<td><img src='data:" + visTopPlantID2url.get(p).toString() + "'/></td>");
+				else
+					sb.append("<td><td/>");
+				LinkedHashMap<String, Boolean> traitChange = plantid2property2topOrLowPercentile.get(p);
+				for (String trait : traitChange.keySet()) {
+					if (traitChange.get(trait))
+						sb.append("<td bgcolor='#FFEEEE'>high " + trait + "</td>");
+					else
+						sb.append("<td bgcolor='#EEEEFF'>low " + trait + "</td>");
+				}
+				sb.append("</tr>");
 			}
-			sb.append("</tr>");
-		}
-		sb.append("</table>");
-		calculationResults.add(sb.toString());
+			sb.append("</table>");
+			calculationResults.add(sb.toString());
+		} else
+			calculationResults.add("Found no plants with outlier traits for selected day and alpha level.");
 	}
 	
 	@Override
@@ -199,6 +314,7 @@ public class OutlierAnalysis extends AbstractNavigationAction implements ActionD
 	
 	@Override
 	public MainPanelComponent getResultMainPanel() {
+		Handler.install();
 		return new MainPanelComponent(calculationResults);
 	}
 	
