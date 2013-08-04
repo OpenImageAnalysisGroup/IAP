5efd964034fff62b4c294f9f1bfad91758e0a7a8
Dr. Christian Klukas <cklukas@users.noreply.github.com>
2013-08-04 16:42:58 +0000

    Renamed post processing method, to reflect that the it works on different kind of images, not only on visible light images.

--- IAP/src/de/ipk/ag_ba/image/operations/blocks/properties/RunnableOnImageSet.java
+++ IAP/src/de/ipk/ag_ba/image/operations/blocks/properties/RunnableOnImageSet.java
@@ -4,7 +4,7 @@ import de.ipk.ag_ba.image.structures.Image;
 import de.ipk.ag_ba.server.analysis.ImageConfiguration;
 
 public interface RunnableOnImageSet {
-	public Image postProcessVis(Image vis);
+	public Image postProcessImage(Image vis);
 
 	public ImageConfiguration getConfig();
 }
