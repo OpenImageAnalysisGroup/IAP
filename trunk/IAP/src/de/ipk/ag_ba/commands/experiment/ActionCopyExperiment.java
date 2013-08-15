45501196052ab14e082c0ebf4718422e51fb4d19
klukas <klukas@90d7db82-dce4-4103-b106-19f43c5924aa>
2013-08-15 15:18:40 +0000

    Optionally skip all defined outliers (global definition and individual annotation definition) during copy operation.

--- trunk/IAP/src/de/ipk/ag_ba/commands/experiment/ActionCopyExperiment.java
+++ trunk/IAP/src/de/ipk/ag_ba/commands/experiment/ActionCopyExperiment.java
@@ -2,12 +2,15 @@ package de.ipk.ag_ba.commands.experiment;
 
 import java.util.ArrayList;
 
+import org.MeasurementFilter;
 import org.SettingsHelperDefaultIsFalse;
+import org.graffiti.plugin.algorithm.ThreadSafeOptions;
 
 import de.ipk.ag_ba.commands.AbstractNavigationAction;
 import de.ipk.ag_ba.commands.experiment.hsm.ActionDataUdpBroadcast;
 import de.ipk.ag_ba.commands.experiment.view_or_export.ActionDataProcessing;
 import de.ipk.ag_ba.commands.mongodb.ActionCopyListOfExperimentsToMongo;
+import de.ipk.ag_ba.commands.settings.ActionToggle;
 import de.ipk.ag_ba.commands.vfs.ActionDataExportToVfs;
 import de.ipk.ag_ba.commands.vfs.VirtualFileSystem;
 import de.ipk.ag_ba.commands.vfs.VirtualFileSystemFolderStorage;
@@ -17,9 +20,16 @@ import de.ipk.ag_ba.gui.interfaces.NavigationAction;
 import de.ipk.ag_ba.gui.navigation_model.GUIsetting;
 import de.ipk.ag_ba.gui.navigation_model.NavigationButton;
 import de.ipk.ag_ba.gui.util.ExperimentReference;
+import de.ipk.ag_ba.gui.util.IAPservice;
 import de.ipk.ag_ba.mongo.MongoDB;
 import de.ipk_gatersleben.ag_nw.graffiti.plugins.gui.editing_tools.script_helper.ExperimentHeaderInterface;
+import de.ipk_gatersleben.ag_nw.graffiti.plugins.gui.editing_tools.script_helper.ExperimentInterface;
+import de.ipk_gatersleben.ag_nw.graffiti.plugins.gui.editing_tools.script_helper.NumericMeasurementInterface;
+import de.ipk_gatersleben.ag_nw.graffiti.plugins.gui.editing_tools.script_helper.SampleInterface;
 import de.ipk_gatersleben.ag_nw.graffiti.plugins.gui.layout_control.network.TabAglet;
+import de.ipk_gatersleben.ag_pbi.mmd.experimentdata.MeasurementNodeType;
+import de.ipk_gatersleben.ag_pbi.mmd.experimentdata.Sample3D;
+import de.ipk_gatersleben.ag_pbi.mmd.experimentdata.Substance3D;
 
 /**
  * @author klukas
@@ -33,6 +43,8 @@ public class ActionCopyExperiment extends AbstractNavigationAction implements Na
 	// private boolean addHSMcopy;
 	private ArrayList<VirtualFileSystem> vl;
 	private boolean addUDPcopy;
+	private int outlierCountNumerics;
+	private int outlierCountBinary;
 	
 	public ActionCopyExperiment(String tooltip) {
 		super(tooltip);
@@ -60,6 +72,23 @@ public class ActionCopyExperiment extends AbstractNavigationAction implements Na
 		this.addUDPcopy = new SettingsHelperDefaultIsFalse().isEnabled(TabAglet.ENABLE_BROADCAST_SETTING);
 		// this.addHSMcopy = new SettingsHelperDefaultIsTrue().isEnabled("ARCHIVE|enabled");
 		this.vl = VirtualFileSystemFolderStorage.getKnown(true);
+		this.outlierCountNumerics = 0;
+		this.outlierCountBinary = 0;
+		if (experimentReferences.size() == 1) {
+			MeasurementFilter mf = IAPservice.getMeasurementFilter(experimentReferences.get(0).getHeader());
+			
+			ExperimentInterface e = experimentReferences.get(0).getData(getStatusProvider());
+			for (SampleInterface s : Substance3D.getAllSamples(e)) {
+				for (NumericMeasurementInterface binary : ((Sample3D) s).getMeasurements(MeasurementNodeType.binaryTypes())) {
+					if (mf.isGlobalOutlierOrSpecificOutlier(binary))
+						outlierCountBinary++;
+				}
+				for (NumericMeasurementInterface numeric : ((Sample3D) s).getMeasurements(MeasurementNodeType.OMICS)) {
+					if (mf.isGlobalOutlierOrSpecificOutlier(numeric))
+						outlierCountNumerics++;
+				}
+			}
+		}
 	}
 	
 	@Override
@@ -72,12 +101,27 @@ public class ActionCopyExperiment extends AbstractNavigationAction implements Na
 	@Override
 	public ArrayList<NavigationButton> getResultNewActionSet() {
 		ArrayList<NavigationButton> res = new ArrayList<NavigationButton>();
+		ThreadSafeOptions includeOutliers = new ThreadSafeOptions();
+		if (experimentReferences.size() == 1 && (outlierCountBinary > 0 || outlierCountNumerics > 0)) {
+			String name = "Skipping Outliers";
+			if (outlierCountNumerics > 0 && outlierCountBinary > 0)
+				name = name + "<br>(" + outlierCountNumerics + " numerics, " + outlierCountBinary + " files)";
+			else
+				if (outlierCountNumerics > 0)
+					name = name + "<br>(" + outlierCountNumerics + " numeric value" + (outlierCountNumerics > 1 ? "s" : "") + ")";
+				else
+					name = name + "<br>(" + outlierCountBinary + " file" + (outlierCountBinary > 1 ? "s" : "") + ")";
+			ActionToggle removeOutliers =
+					new ActionToggle("Remove outliers during copy?", "Complete Copy<br>(outliers included)", name, includeOutliers);
+			
+			res.add(new NavigationButton(removeOutliers, guiSetting));
+		}
 		for (MongoDB m : ml)
-			res.add(new NavigationButton(new ActionCopyListOfExperimentsToMongo(m, experimentReferences), guiSetting));
+			res.add(new NavigationButton(new ActionCopyListOfExperimentsToMongo(m, experimentReferences, false, !includeOutliers.getBval(0, false)), guiSetting));
 		for (VirtualFileSystem vx : vl) {
 			if (vx instanceof VirtualFileSystemVFS2) {
 				VirtualFileSystemVFS2 v = (VirtualFileSystemVFS2) vx;
-				res.add(new NavigationButton(new ActionDataExportToVfs(m, experimentReferences, v), guiSetting));
+				res.add(new NavigationButton(new ActionDataExportToVfs(m, experimentReferences, v, !includeOutliers.getBval(0, false)), guiSetting));
 			}
 		}
 		
@@ -95,11 +139,11 @@ public class ActionCopyExperiment extends AbstractNavigationAction implements Na
 		
 		res.add(new NavigationButton(new ActionDataExportToUserSelectedFileSystemFolder(
 				"Copy dataset to a user-selected target folder",
-				m, experimentReferences), guiSetting));
+				m, experimentReferences, !includeOutliers.getBval(0, false)), guiSetting));
 		
 		res.add(new NavigationButton(new ActionDataExportToUserSelectedVFStarget(
 				"Copy dataset to a user-selected remote target",
-				m, experimentReferences), guiSetting));
+				m, experimentReferences, !includeOutliers.getBval(0, false)), guiSetting));
 		
 		return res;
 	}
