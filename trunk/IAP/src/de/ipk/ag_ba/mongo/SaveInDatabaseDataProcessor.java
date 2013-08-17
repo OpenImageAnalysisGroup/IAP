3c9341ed10c3f7dafd61c6886151874bfd45dba3
klukas <klukas@90d7db82-dce4-4103-b106-19f43c5924aa>
2013-08-17 15:10:18 +0000

    Only writeable VFS locations are listed, avoiding possibility of selection of readonly data sources.

--- trunk/IAP/src/de/ipk/ag_ba/mongo/SaveInDatabaseDataProcessor.java
+++ trunk/IAP/src/de/ipk/ag_ba/mongo/SaveInDatabaseDataProcessor.java
@@ -41,7 +41,7 @@ public class SaveInDatabaseDataProcessor extends AbstractExperimentDataProcessor
 			Object[] sel = MyInputHelper.getInput("Select the database-target:", "Storage System", new Object[] {
 					"MongoDB", MongoDB.getMongos(),
 					"Store not in MongoDB but in VFS?", false,
-					"VFS", VirtualFileSystem.getKnown(true)
+					"VFS", VirtualFileSystem.getKnown(true, true)
 			});
 			
 			if (sel == null)
