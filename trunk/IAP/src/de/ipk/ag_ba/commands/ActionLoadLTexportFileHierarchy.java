238fc3aaf0ddf56b9234f574370fd097e0a56811
klukas <klukas@90d7db82-dce4-4103-b106-19f43c5924aa>
2013-08-17 15:11:19 +0000

    Added information message with hints on how to proceed (checking meta data and copying the data set).

--- trunk/IAP/src/de/ipk/ag_ba/commands/ActionLoadLTexportFileHierarchy.java
+++ trunk/IAP/src/de/ipk/ag_ba/commands/ActionLoadLTexportFileHierarchy.java
@@ -199,15 +199,19 @@ public class ActionLoadLTexportFileHierarchy extends AbstractNavigationAction {
 					}
 				}
 		}
-		messages.add("INFO: Snapshot-count: " + snapshots.size());
+		messages.add("Processed " + snapshots.size() + " exported snapshots.");
+		messages.add("Next steps:");
+		messages
+				.add("<ol><li><b>Open dataset(s) and check the correct assignment of meta-data (species/genotype/treatment/...) with the 'View Data' command.</b><br>If not correct, open another window (right click any command bar at the top, and go to 'Settings &gt; Metadata &gt; Columns'. Check the content of the metadata file, stored in the location of the exported snapshots, and compare the metadata prepared metadata table columns with the IAP input metadata settings. Consult the documentation for further information.");
+		messages.add("<li><b>For further processing the created datasets need to be copied to a storage site (MongoDB database / local folder / VFS)!</b></ol>");
 		getStatusProvider().setCurrentStatusValueFine(0d);
 		getStatusProvider().setCurrentStatusText1("Found " + snapshots.size() + " data points");
 		getStatusProvider().setCurrentStatusText2("(images, weight or watering info)");
 		HashMap<String, Condition> optIdTag2condition = null;
 		if (!foundMetadata || metadata == null) {
-			messages.add("<b>Found no meta-data files (files ending with file extension name .csv, .xlsx or .xls) in the selected folder!</b>");
-			messages.add("Check the 'File Import'-Settings for the meta-data assignment of column-values!");
-			messages.add("Modify the settings and repeat the loading, in case data is not assigned to the pre-defined fields as desired.");
+			messages.add("<b>Found no meta-data files (files ending with file extension name .csv, .xlsx or .xls) in the selected folder!</b><br>" +
+					"Check the 'File Import'-Settings for the meta-data assignment of column-values!<br>" +
+					"Modify the settings and repeat the loading, in case data is not assigned to the pre-defined fields as desired.");
 		} else {
 			// process metadata
 			optIdTag2condition = new HashMap<String, Condition>();
@@ -281,7 +285,7 @@ public class ActionLoadLTexportFileHierarchy extends AbstractNavigationAction {
 	
 	@Override
 	public MainPanelComponent getResultMainPanel() {
-		return new MainPanelComponent("<b>Messages:</b><br><br>" + StringManipulationTools.getStringList(messages, "<br>"));
+		return new MainPanelComponent("<b>Results</b><br><br><hr><br>" + StringManipulationTools.getStringList(messages, "<br><br>"));
 	}
 	
 	@Override
