6f5237b8b26a244dc7918ade017e8479c3e2d520
klukas <klukas@90d7db82-dce4-4103-b106-19f43c5924aa>
2013-08-25 20:07:26 +0000

    'Save annotation changes' command implemented for VFS locations, changes are saved in-place.

--- trunk/IAP/src/de/ipk/ag_ba/commands/experiment/ActionViewExportData.java
+++ trunk/IAP/src/de/ipk/ag_ba/commands/experiment/ActionViewExportData.java
@@ -4,12 +4,17 @@ import java.util.ArrayList;
 
 import org.ErrorMsg;
 import org.graffiti.plugin.algorithm.ThreadSafeOptions;
+import org.graffiti.plugin.io.resources.ResourceIOHandler;
+import org.graffiti.plugin.io.resources.ResourceIOManager;
 
 import de.ipk.ag_ba.commands.AbstractNavigationAction;
 import de.ipk.ag_ba.commands.experiment.view_or_export.ActionDataExportTar;
 import de.ipk.ag_ba.commands.experiment.view_or_export.ActionDataExportZIP;
 import de.ipk.ag_ba.commands.experiment.view_or_export.ActionDataProcessing;
 import de.ipk.ag_ba.commands.mongodb.ActionCopyToMongo;
+import de.ipk.ag_ba.commands.vfs.ActionDataExportToVfs;
+import de.ipk.ag_ba.commands.vfs.VirtualFileSystemHandler;
+import de.ipk.ag_ba.commands.vfs.VirtualFileSystemVFS2;
 import de.ipk.ag_ba.gui.MainPanelComponent;
 import de.ipk.ag_ba.gui.navigation_model.NavigationButton;
 import de.ipk.ag_ba.gui.picture_gui.SupplementaryFilePanelMongoDB;
@@ -56,8 +61,71 @@ public class ActionViewExportData extends AbstractNavigationAction implements Ac
 		// todo add zoom slider (default, large, extra large)
 		// todo add plant filter (all, ID 1, ID 2, ID 3, ...)
 		
-		res.add(new NavigationButton("Save Annotation Changes", new ActionCopyToMongo(experiment.m, experiment, true), src.getGUIsetting()));
-		
+		if (experiment.m != null)
+			res.add(new NavigationButton("Save Annotation Changes", new ActionCopyToMongo(experiment.m, experiment, true), src.getGUIsetting()));
+		else {
+			String dbId = experiment != null && experiment.getHeader() != null ? experiment.getHeader().getDatabaseId() : null;
+			if (dbId != null) {
+				String id = dbId.contains(":") ? dbId.substring(0, dbId.indexOf(":")) : null;
+				if (id != null && !id.isEmpty()) {
+					ResourceIOHandler vfs = ResourceIOManager.getHandlerFromPrefix(id);
+					if (vfs instanceof VirtualFileSystemHandler) {
+						VirtualFileSystemHandler vv = (VirtualFileSystemHandler) vfs;
+						if (vv.getVFS() instanceof VirtualFileSystemVFS2) {
+							VirtualFileSystemVFS2 vv2 = (VirtualFileSystemVFS2) vv.getVFS();
+							if (vv2.isAbleToSaveData())
+								res.add(new NavigationButton(null,
+										new ActionDataExportToVfs(experiment.m, experiment, vv2, false, null) {
+											
+											@Override
+											public void performActionCalculateResults(NavigationButton src) throws Exception {
+												setSkipClone(true);
+												setSkipUpdateDBid(true);
+												super.performActionCalculateResults(src);
+											}
+											
+											@Override
+											public boolean isProvidingActions() {
+												return true;
+											}
+											
+											@Override
+											public boolean requestTitleUpdates() {
+												return false;
+											}
+											
+											@Override
+											public String getDefaultTitle() {
+												String res = "Save Annotation Changes" + (postResult != null ? "<br>" + postResult : "");
+												return res;
+											}
+											
+											@Override
+											public ArrayList<NavigationButton> getResultNewActionSet() {
+												return null;
+											}
+											
+											@Override
+											public MainPanelComponent getResultMainPanel() {
+												return null;
+											}
+											
+											@Override
+											public ArrayList<NavigationButton> getResultNewNavigationSet(ArrayList<NavigationButton> currentSet) {
+												return currentSet;
+											}
+											
+											@Override
+											public String getDefaultImage() {
+												return "img/ext/gpl2/Gnome-Emblem-Downloads-64.png";
+											}
+										},
+										src.getGUIsetting()));
+						}
+					}
+				}
+			}
+		}
 		final ArrayList<ThreadSafeOptions> toggles = new ArrayList<ThreadSafeOptions>();
 		res.add(new NavigationButton(new ActionNumericExportCommands(
 				"Export Numeric Data", toggles, experiment), src.getGUIsetting()));
