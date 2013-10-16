aa3dfcbbc875d039ae2615be98367141ed0aab94
Dr. Christian Klukas <cklukas@users.noreply.github.com>
2013-10-16 05:31:07 +0000

    IAP V1.0.0

---
+++ Graffiti_Plugins/org/graffiti/plugins/views/defaults/DrawMode.java
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2003-2009 Plant Bioinformatics Group, IPK Gatersleben
+ *******************************************************************************/
+/*
+ * Created on Nov 18, 2009 by Christian Klukas
+ */
+package org.graffiti.plugins.views.defaults;
+
+/**
+ * @author klukas
+ */
+public enum DrawMode {
+	NORMAL, REDUCED
+	
+}
