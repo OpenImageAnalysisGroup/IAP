183b7f610e8f6613ff432f6729e3e6568ace8ff6
klukas <klukas@90d7db82-dce4-4103-b106-19f43c5924aa>
2014-05-14 12:30:05 +0000

    Add simple overlay program (to mark images on file system as not available).

---
+++ branches/IAP_v1.1.x/IAP/src/iap/Overlay.java
@@ -0,0 +1,45 @@
+package iap;
+
+import java.awt.Color;
+import java.io.File;
+
+import org.graffiti.plugin.io.resources.AbstractResourceIOHandler;
+import org.graffiti.plugin.io.resources.FileSystemHandler;
+
+import de.ipk.ag_ba.image.structures.Image;
+
+/**
+ * @author Christian Klukas
+ */
+
+public class Overlay {
+	public static void main(String[] args) {
+		try {
+			String url = args[0];
+			AbstractResourceIOHandler ha = new FileSystemHandler();
+			Image image = new Image(ha.getInputStream(FileSystemHandler.getURL(new File(url))));
+			
+			int x = 0, y = 0, w = image.getWidth(), h = image.getHeight();
+			double alpha = 0.5;
+			int color = new Color(50, 50, 50).getRGB();
+			
+			// Color lr = new Color(255, 127, 127);
+			Color lr = new Color(180, 180, 180);
+			int lri = lr.getRGB();
+			
+			image = image.io().canvas().fillRect(x, y, w, h, color, alpha).getImage().io().addBorder(20, 0, 0, lri)
+					.drawLine(0, 0, w + 20 * 2, h + 20 * 2, lr, 20).drawLine(w + 20 * 2, 0, 0, h + 20 * 2, lr, 20).addBorder(5, 0, 0, lri)
+					.canvas()
+					.text((int) (180d / 700d * w) + 4, (int) (310d / 530d * h) + 4, "Webcam Offline", Color.DARK_GRAY, 40)
+					.text((int) (180d / 700d * w), (int) (310d / 530d * h), "Webcam Offline", new Color(255, 127, 127), 40)
+					.getImage();
+			
+			image.saveToFile(args[1]);
+			
+			System.exit(0);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+}
