9832fdbcaae89eacff4a52fe7ed065cb7d15bcf7
j-mp <j-mp@90d7db82-dce4-4103-b106-19f43c5924aa>
2016-02-09 14:18:35 +0000

    Fix BlHSV filter (repeat function removed) and BlMorphologicalOperations (Fluo and Nir fixed).

--- trunk/IAP/src/iap/blocks/segmentation/BlFilterByHSV.java
+++ trunk/IAP/src/iap/blocks/segmentation/BlFilterByHSV.java
@@ -3,15 +3,14 @@
  */
 package iap.blocks.segmentation;
 
-import iap.blocks.data_structures.AbstractSnapshotAnalysisBlock;
-import iap.blocks.data_structures.BlockType;
-
 import java.util.HashSet;
 
 import de.ipk.ag_ba.image.operation.ImageOperation;
 import de.ipk.ag_ba.image.structures.CameraType;
 import de.ipk.ag_ba.image.structures.Image;
 import de.ipk.ag_ba.image.structures.ImageStack;
+import iap.blocks.data_structures.AbstractSnapshotAnalysisBlock;
+import iap.blocks.data_structures.BlockType;
 
 /**
  * Uses a HSV-based pixel filter(s) for the vis/fluo/nir images.
@@ -40,60 +39,57 @@ public class BlFilterByHSV extends AbstractSnapshotAnalysisBlock {
 			return mask;
 		else {
 			boolean debug = false;
+			boolean debug_1 = getBoolean("Debug Mask Manipulation", false);
 			ImageOperation processedMask = mask.io().show("in mask", debug).copy();
-			int HSVfilters = getInt("Number of HSV " + optics + " filters", 1);
+			// int HSVfilters = getInt("Number of HSV " + optics + " filters", 1);
+			int HSVfilters = 1;
 			Image imageUnChanged = image.copy();
 			ImageStack st = null;
-			if (getBoolean("Debug Mask Manipulation", false))
-				st = new ImageStack();
+			if (debug_1)
+			st = new ImageStack();
 			for (int filter = 1; filter <= HSVfilters; filter++) {
 				String pf = optics + " filter " + filter + " ";
 				ImageOperation blurred = processedMask.blurImageJ(getDouble(pf + " blur", 1)).show("in mask blurred", debug);
-				boolean manip = getBoolean(pf + "manipulate mask", false);
-				if (!manip) {
-					processedMask = blurred.filterRemoveHSV(
-							getDouble(pf + "min H", (2 / 3d) - (1 / 2d - 1 / 3d)),
-							getDouble(pf + "max H", (2 / 3d) + (1 / 2d - 1 / 3d)),
-							getDouble(pf + "min S", 0),
-							getDouble(pf + "max S", 1),
-							getDouble(pf + "min V", 0),
-							getDouble(pf + "max V", (200d / 255d))).show(pf + " res", debug);
-				} else {
-					ImageOperation filteredContent = blurred.filterRemainHSV(
-							getDouble(pf + "min H", (2 / 3d) - (1 / 2d - 1 / 3d)),
-							getDouble(pf + "max H", (2 / 3d) + (1 / 2d - 1 / 3d)),
-							getDouble(pf + "min S", 0),
-							getDouble(pf + "max S", 1),
-							getDouble(pf + "min V", 0),
-							getDouble(pf + "max V", (200d / 255d))).show(pf + " res", false);
-					int dilate = getInt(pf + "mask dilate 1", 0);
-					int erode = getInt(pf + "mask erode 2", 0);
-					int dilate2 = getInt(pf + "mask dilate 3", 0);
-					if (st != null)
-						st.addImage("mask " + filter, filteredContent.getImage());
-					if (dilate > 0)
-						filteredContent = filteredContent.bm().dilate(dilate).io();
-					if (erode > 0)
-						filteredContent = filteredContent.bm().erode(erode).io();
-					if (dilate2 > 0)
-						filteredContent = filteredContent.bm().dilate(dilate2).io();
-					if (st != null)
-						st.addImage("mask " + filter + ", modified", filteredContent.getImage());
-					processedMask.show(pf + " processed mask", getBoolean("Debug Mask Manipulation", false));
-					mask.show(pf + " mask", getBoolean("Debug Mask Manipulation", false));
-					processedMask = processedMask.xor(mask).show(pf + " xor res", getBoolean("Debug Mask Manipulation", false));
-					mask = processedMask.getImage();
+				
+				ImageOperation filteredContent = blurred.filterRemainHSV(
+						getDouble(pf + "min H", (2 / 3d) - (1 / 2d - 1 / 3d)),
+						getDouble(pf + "max H", (2 / 3d) + (1 / 2d - 1 / 3d)),
+						getDouble(pf + "min S", 0),
+						getDouble(pf + "max S", 1),
+						getDouble(pf + "min V", 0),
+						getDouble(pf + "max V", (200d / 255d))).show(pf + " res", false);
+				int dilate = getInt(pf + "mask dilate 1", 0);
+				int erode = getInt(pf + "mask erode 2", 0);
+				int dilate2 = getInt(pf + "mask dilate 3", 0);
+				if (st != null)
+					st.addImage("mask " + filter, filteredContent.getImage());
+				if (dilate > 0)
+					filteredContent = filteredContent.bm().dilate(dilate).io();
+				if (erode > 0)
+					filteredContent = filteredContent.bm().erode(erode).io();
+				if (dilate2 > 0)
+					filteredContent = filteredContent.bm().dilate(dilate2).io();
+				if (st != null)
+					st.addImage("mask " + filter + ", modified", filteredContent.getImage());
+					
+				if (debug_1) {
+					processedMask.show(pf + " processed mask");
+					mask.show(pf + " mask");
 				}
+	
+				processedMask = processedMask.xor(mask).show(pf + " xor res", debug_1);
+				
 				processedMask = processedMask.and(mask);
-				image = image.io().applyMask(
-						processedMask.closing(getInt(pf + "dilate", 2), getInt(pf + "erode", 4)).getImage(),
-						optionsAndResults.getBackground()).getImage();
+				// image = image.io().applyMask(
+				// processedMask.closing(getInt(pf + "dilate", 2), getInt(pf + "erode", 4)).getImage(),
+				// optionsAndResults.getBackground()).getImage();
 			}
 			if (st != null && st.size() > 0)
 				st.show("Debug mask");
 			// blur introduces new pixel areas, so the original mask is applied here, to shrink it down
 			// so that the result does not introduce new pixel areas, only less (filter operation)
 			processedMask = imageUnChanged.io().applyMask(processedMask.getImage(), optionsAndResults.getBackground());
+
 			return processedMask.getImage();
 		}
 	}
--- trunk/IAP/src/iap/blocks/segmentation/BlMorphologicalOperations.java
+++ trunk/IAP/src/iap/blocks/segmentation/BlMorphologicalOperations.java
@@ -1,13 +1,12 @@
 package iap.blocks.segmentation;
 
-import iap.blocks.data_structures.AbstractBlock;
-import iap.blocks.data_structures.BlockType;
-
 import java.util.HashSet;
 
 import de.ipk.ag_ba.image.operation.ImageOperation;
 import de.ipk.ag_ba.image.structures.CameraType;
 import de.ipk.ag_ba.image.structures.Image;
+import iap.blocks.data_structures.AbstractBlock;
+import iap.blocks.data_structures.BlockType;
 
 /**
  * Analysis block for morphological image operations. 3 Steps are performed, first erode, then dilate, and then erode again. Each step can be performed multiple
@@ -40,14 +39,15 @@ public class BlMorphologicalOperations extends AbstractBlock {
 	
 	@Override
 	protected Image processMask(Image mask) {
+		// set default enabled for vis analysis
 		if (mask != null && getBoolean("Process " + mask.getCameraType(), mask.getCameraType() == CameraType.VIS)) {
 			ImageOperation binaryMask = mask.io();
 			int er1 = getInt(mask.getCameraType() + " Step 1 Erode Count", 1);
 			int di1 = getInt(mask.getCameraType() + " Step 2 Dilate Count", 2);
 			binaryMask = binaryMask.bm().erode(getRoundMask(er1))
-					.dilate(getRoundMask(di1))
-					.erode(getRoundMask(getInt(mask.getCameraType() + " Step 3 Erode Count", 0))).io();
-			Image orig = input().images().vis();
+				.dilate(getRoundMask(di1))
+				.erode(getRoundMask(getInt(mask.getCameraType() + " Step 3 Erode Count", 0))).io();
+			Image orig = input().images().getImage(mask.getCameraType());
 			Image img = orig.copy().io().applyMask(binaryMask.getImage(), ImageOperation.BACKGROUND_COLORint).getImage();
 			return img;
 		} else
@@ -61,12 +61,12 @@ public class BlMorphologicalOperations extends AbstractBlock {
 		double m = size / 2d;
 		for (int x = 0; x < size; x++)
 			for (int y = 0; y < size; y++) {
-				double cX = x - m + 0.5;
-				double cY = y - m + 0.5;
-				double d = Math.sqrt(cX * cX + cY * cY);
-				boolean inside = d <= m - 0.25;
-				int insideCircle = inside ? 1 : 0xffffffff;
-				kernel[x][y] = insideCircle;
+			double cX = x - m + 0.5;
+			double cY = y - m + 0.5;
+			double d = Math.sqrt(cX * cX + cY * cY);
+			boolean inside = d <= m - 0.25;
+			int insideCircle = inside ? 1 : 0xffffffff;
+			kernel[x][y] = insideCircle;
 			}
 		return kernel;
 	}
@@ -79,9 +79,9 @@ public class BlMorphologicalOperations extends AbstractBlock {
 	@Override
 	public String getDescription() {
 		return "Analysis block for morphological image operations. 3 Steps are performed, first erode, then dilate, " +
-				"and then erode again. " +
-				"Each step can be performed multiple times, according to user preference. " +
-				"This way this block can perform the Opening or Closing operation, depending on its settings, " +
-				"or only Erode or only Dilate.";
+			"and then erode again. " +
+			"Each step can be performed multiple times, according to user preference. " +
+			"This way this block can perform the Opening or Closing operation, depending on its settings, " +
+			"or only Erode or only Dilate.";
 	}
 }
